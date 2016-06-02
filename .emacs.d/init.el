914922349fd5e802ab3a5f08c01fe69f05b8cb88
masa <latestmasa@gmail.com>
2016-06-02 10:51:48 +0900

    全角文字と半角英数字の間に自動で半角スペースを挿入できるように

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -368,6 +368,21 @@
   (define-key go-mode-map (kbd "M-,") 'pop-tag-mark))
 
 
+
+;;; 全角文字と半角英字の間に半角スペースを入れる
+(setq pangu-spacing-chinese-before-english-regexp
+  (rx (group-n 1 (category japanese))
+      (group-n 2 (in "a-zA-Z0-9"))))
+(setq pangu-spacing-chinese-after-english-regexp
+  (rx (group-n 1 (in "a-zA-Z0-9"))
+      (group-n 2 (category japanese))))
+;;; 見た目ではなくて実際にスペースを入れる
+(setq pangu-spacing-real-insert-separtor t)
+;; markdown-modeで利用する
+(add-hook 'markdown-mode-hook 'pangu-spacing-mode)
+
+
+
 (provide 'init)
 ;;; init.el ends here
 
