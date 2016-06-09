fdeebbebb87dc7f5aeaa6098c58ce224ad261f0f
masa <latestmasa@gmail.com>
2016-06-09 16:53:31 +0900

    arch linux用に変更

--- symlink.sh
+++ symlink.sh
@@ -3,6 +3,7 @@ ln -sf ~/git/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/git/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/git/dotfiles/.screenrc ~/.screenrc
 ln -sf ~/git/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/git/dotfiles/.xinitrc ~/.xinitrc
 ln -sfn ~/git/dotfiles/.peco ~/.peco
 ln -sfn ~/git/dotfiles/.emacs.d ~/.emacs.d
 ln -sfn ~/Dropbox/ssh ~/.ssh
