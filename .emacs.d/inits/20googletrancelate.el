ffbddd98433b8c70d33a0b9cefce018b9e5f4163
masa <latestmasa@gmail.com>
2016-06-21 18:10:06 +0900

    書き直した

--- .emacs.d/inits/20googletrancelate.el
+++ .emacs.d/inits/20googletrancelate.el
@@ -1,8 +1,6 @@
 (require 'google-translate)
 (require 'google-translate-default-ui)
 
-(defvar google-translate-english-chars "[:ascii:]"
-  "これらの文字が含まれているときは英語とみなす")
 (defun google-translate-enja-or-jaen (&optional string)
   "regionか現在位置の単語を翻訳する。C-u付きでquery指定も可能"
   (interactive)
@@ -15,7 +13,7 @@
               (t
                (thing-at-point 'word))))
   (let* ((asciip (string-match
-                  (format "\\`[%s]+\\'" google-translate-english-chars)
+                  (format "\\`[%s]+\\'" "[:ascii:]")
                   string)))
     (run-at-time 0.1 nil 'deactivate-mark)
     (google-translate-translate
