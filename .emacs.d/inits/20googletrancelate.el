b00b620eaf529992092f3195c150b585ac03e789
masa <latestmasa@gmail.com>
2016-06-21 18:00:55 +0900

    日本語から英語に対応

--- .emacs.d/inits/20googletrancelate.el
+++ .emacs.d/inits/20googletrancelate.el
@@ -1,13 +1,28 @@
-;; google-translate
 (require 'google-translate)
-(use-package google-translate
-  :config
-  (global-set-key (kbd "C-c t") 'google-translate-at-point)
+(require 'google-translate-default-ui)
 
-  ; en -> ja
-  (custom-set-variables
-    '(google-translate-default-source-language "en")
-    '(google-translate-default-target-language "ja"))
+(defvar google-translate-english-chars "[:ascii:]"
+  "これらの文字が含まれているときは英語とみなす")
+(defun google-translate-enja-or-jaen (&optional string)
+  "regionか現在位置の単語を翻訳する。C-u付きでquery指定も可能"
+  (interactive)
+  (setq string
+        (cond ((stringp string) string)
+              (current-prefix-arg
+               (read-string "Google Translate: "))
+              ((use-region-p)
+               (buffer-substring (region-beginning) (region-end)))
+              (t
+               (thing-at-point 'word))))
+  (let* ((asciip (string-match
+                  (format "\\`[%s]+\\'" google-translate-english-chars)
+                  string)))
+    (run-at-time 0.1 nil 'deactivate-mark)
+    (google-translate-translate
+     (if asciip "en" "ja")
+     (if asciip "ja" "en")
+     string)))
 
-  (with-eval-after-load 'popwin
-    (push "*Google Translate*" popwin:special-display-config)))
+(push "*Google Translate*" popwin:special-display-config)
+
+(global-set-key (kbd "C-c t") 'google-translate-enja-or-jaen)
