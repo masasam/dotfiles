259a0f37152736c6a0195e0bd938595a5261339c
masa <archmasa@users.noreply.github.com>
2016-09-05 08:57:39 +0900

    TODOをorg-modeでやるように変更

--- .emacs.d/inits/05org.el
+++ .emacs.d/inits/05org.el
@@ -11,3 +11,16 @@
 (setq org-log-done 'time)
 
 (global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/Dropbox/emacs/todo.org" "Tasks")
+             "* TODO %? %i %a")
+        ("j" "Journal" entry (file+datetree "~/Dropbox/emacs/journal.org")
+             "* %? %U %i %a")
+        ("n" "Note" entry (file+headline "~/Dropbox/emacs/notes.org" "Notes")
+	 "* %? %U %i")
+	("i" "interrupted task" entry
+         (file "~/Dropbox/emacs/todo.org")
+         "* %?" :clock-in t :clock-resume t)
+         ))
