6faa228b94497d16b79a4f0163820fb28494ea3c
masa <archmasa@users.noreply.github.com>
2016-09-05 05:47:50 +0900

    which-keyを導入

---
+++ .emacs.d/inits/06whichkey.el
@@ -0,0 +1,6 @@
+;;; 3つの表示方法どれか1つ選ぶ
+(which-key-setup-side-window-bottom)    ;ミニバッファ
+;; (which-key-setup-side-window-right)     ;右端
+;; (which-key-setup-side-window-right-bottom) ;両方使う
+
+(which-key-mode 1)
