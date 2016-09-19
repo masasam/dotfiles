920664b240e94ee93887f05fb6d303294df15a67
masa <archmasa@users.noreply.github.com>
2016-09-19 10:58:52 +0900

    emacs25.1動作確認とhelmの見た目変更

--- .emacs.d/inits/99_custom.el
+++ .emacs.d/inits/99_custom.el
@@ -4,5 +4,5 @@
 ;;;; themeの一部分を自分好みに変更 M-x list-faces-display で調べて変更する
 (custom-set-faces
  '(fixed-pitch ((t (:family "Ricty"))))
- '(helm-candidate-number ((t (:background "cornflower blue" :foreground "#ffffff"))))
+ '(helm-candidate-number ((t (:background "#1c1f26" :foreground "#ffffff"))))
  '(variable-pitch ((t (:family "Ricty")))))
