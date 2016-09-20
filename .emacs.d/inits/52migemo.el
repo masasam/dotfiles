4f59e8603cb414f4d897050a715ae76ce3012a1e
masa <archmasa@users.noreply.github.com>
2016-09-20 19:16:52 +0900

    migemoをインストール

---
+++ .emacs.d/inits/52migemo.el
@@ -0,0 +1,11 @@
+(require 'migemo)
+(setq migemo-command "cmigemo")
+(setq migemo-options '("-q" "--emacs"))
+
+;; Set your installed path
+(setq migemo-dictionary "/usr/share/migemo/utf-8/migemo-dict")
+
+(setq migemo-user-dictionary nil)
+(setq migemo-regex-dictionary nil)
+(setq migemo-coding-system 'utf-8-unix)
+(migemo-init)
