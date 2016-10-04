c844328cac7835235a5fba4ea2742849131c5c9c
masa <masasam@users.noreply.github.com>
2016-10-04 18:43:55 +0900

    対応する括弧にvimっぽく飛べるようにした

---
+++ .emacs.d/inits/04matchparen.el
@@ -0,0 +1,9 @@
+;; %で対応する括弧へカーソルを移動できます．カーソ ルが括弧の上になければ，単に%が挿入
+(global-set-key "%" 'match-paren)
+
+(defun match-paren (arg)
+  "Go to the matching paren if on a paren; otherwise insert %."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
