dcbecf1f2556f3e6a56283ac3f1dd94aa4d7023e
masa <masasam@users.noreply.github.com>
2016-11-01 00:08:36 +0900

    direxをインストール

---
+++ .emacs.d/inits/16direx.el
@@ -0,0 +1,22 @@
+;; direx
+(require 'direx)
+(require 'direx-project)
+
+(defun my/dired-jump ()
+  (interactive)
+  (cond (current-prefix-arg
+         (dired-jump))
+        ((not (one-window-p))
+         (or (ignore-errors
+               (direx-project:jump-to-project-root) t)
+             (direx:jump-to-directory)))
+        (t
+         (or (ignore-errors
+               (direx-project:jump-to-project-root-other-window) t)
+             (direx:jump-to-directory-other-window)))))
+
+(global-set-key (kbd "C-x C-j") 'my/dired-jump)
+
+;; widthは環境に合わせて調整してください。
+(push '(direx:direx-mode :position left :width 40 :dedicated t)
+      popwin:special-display-config)
