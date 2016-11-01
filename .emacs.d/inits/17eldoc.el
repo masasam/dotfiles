5f6c6bf986255b67345410a2fc017fc34cfa8ff3
masa <masasam@users.noreply.github.com>
2016-11-01 14:08:46 +0900

    eldoc-extensionをインストール

---
+++ .emacs.d/inits/17eldoc.el
@@ -0,0 +1,7 @@
+(require 'eldoc)
+(require 'eldoc-extension)
+(setq eldoc-idle-delay 0)
+(setq eldoc-echo-area-use-multiline-p t)
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
