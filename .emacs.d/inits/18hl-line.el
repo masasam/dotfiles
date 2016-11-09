41fa63ec20aa3dbf7c767cae9c56862945c07fdf
masa <masasam@users.noreply.github.com>
2016-11-09 13:54:01 +0900

    hl-lineでも遅くならないようになった

---
+++ .emacs.d/inits/18hl-line.el
@@ -0,0 +1,11 @@
+(require 'hl-line)
+;;; hl-lineを無効にするメジャーモードを指定する
+(defvar global-hl-line-timer-exclude-modes '(todotxt-mode))
+(defun global-hl-line-timer-function ()
+  (unless (memq major-mode global-hl-line-timer-exclude-modes)
+    (global-hl-line-unhighlight-all)
+    (let ((global-hl-line-mode t))
+      (global-hl-line-highlight))))
+(setq global-hl-line-timer
+      (run-with-idle-timer 0.03 t 'global-hl-line-timer-function))
+;; (cancel-timer global-hl-line-timer)
