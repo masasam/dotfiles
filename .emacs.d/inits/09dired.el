04338d99c824b6ebe0af70e7a59e8daf85a29dae
masa <masasam@users.noreply.github.com>
2016-11-09 23:36:52 +0900

    reset dirできるようにした

--- .emacs.d/inits/09dired.el
+++ .emacs.d/inits/09dired.el
@@ -1,2 +1,9 @@
 (require 'dired)
 (define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)
+
+(defun my/reset-default-directory-by-buffer-file-name ()
+  "Set default-directory by `buffer-file-name'."
+  (interactive)
+  (require 'f)
+  (when buffer-file-name
+    (setq default-directory (f-dirname buffer-file-name))))
