c477927ccf25f2e1b68902bbd8557bde964b96c3
masa <masasam@users.noreply.github.com>
2016-11-22 17:10:01 +0900

    companyの設定をした

---
+++ .emacs.d/inits/35company.el
@@ -0,0 +1,38 @@
+(global-company-mode +1)
+
+;; 自動補完を offにしたい場合は, company-idle-delayを nilに設定する
+;; auto-completeでいうところの ac-auto-start にあたる.
+(custom-set-variables
+ '(company-idle-delay nil))
+
+(set-face-attribute 'company-tooltip nil
+                    :foreground "black" :background "lightgrey")
+(set-face-attribute 'company-tooltip-common nil
+                    :foreground "black" :background "lightgrey")
+(set-face-attribute 'company-tooltip-common-selection nil
+                    :foreground "white" :background "steelblue")
+(set-face-attribute 'company-tooltip-selection nil
+                    :foreground "black" :background "steelblue")
+(set-face-attribute 'company-preview-common nil
+                    :background nil :foreground "lightgrey" :underline t)
+(set-face-attribute 'company-scrollbar-fg nil
+                    :background "blue")
+(set-face-attribute 'company-scrollbar-bg nil
+                    :background "gray40")
+
+(global-set-key (kbd "C-M-i") 'company-complete)
+
+;; C-n, C-pで補完候補を次/前の候補を選択
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-search-map (kbd "C-n") 'company-select-next)
+(define-key company-search-map (kbd "C-p") 'company-select-previous)
+
+;; C-sで絞り込む
+(define-key company-active-map (kbd "C-s") 'company-filter-candidates)
+
+;; TABで候補を設定
+(define-key company-active-map (kbd "C-i") 'company-complete-selection)
+
+;; 各種メジャーモードでも C-M-iで company-modeの補完を使う
+(define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
