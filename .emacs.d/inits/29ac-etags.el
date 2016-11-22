028e28aa9e0fb0cde7409110cf5e46b0b2bf2290
masa <masasam@users.noreply.github.com>
2016-11-22 13:15:35 +0900

    etagsも補完対象に

---
+++ .emacs.d/inits/29ac-etags.el
@@ -0,0 +1,9 @@
+(custom-set-variables
+  '(ac-etags-requires 1))
+
+(eval-after-load "etags"
+  '(progn
+      (ac-etags-setup)))
+
+(add-hook 'c-mode-common-hook 'ac-etags-ac-setup)
+(add-hook 'ruby-mode-common-hook 'ac-etags-ac-setup)
