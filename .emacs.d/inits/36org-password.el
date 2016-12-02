58b1949689678d7b0a5b205ac3dacc70dc36afb2
masa <masasam@users.noreply.github.com>
2016-12-03 08:15:26 +0900

    password登録関数を利用する

--- .emacs.d/inits/36org-password.el
+++ .emacs.d/inits/36org-password.el
@@ -2,3 +2,8 @@
 
 (require 'org-agenda)
 (push "~/Dropbox/passwd/password.org.gpg" org-agenda-files)
+
+;; password登録
+(defun addpassword ()
+  (interactive)
+  (find-file "~/Dropbox/passwd/password.org.gpg"))
