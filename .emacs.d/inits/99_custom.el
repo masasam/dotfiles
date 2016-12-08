0c28cf5d4cc9b45ebb334584d0f7bdbe933011de
masa <masasam@users.noreply.github.com>
2016-12-08 18:28:02 +0900

    elscreenの色を変更した

--- .emacs.d/inits/99_custom.el
+++ .emacs.d/inits/99_custom.el
@@ -5,7 +5,7 @@
 (custom-set-faces
  '(fixed-pitch ((t (:family "Ricty"))))
  '(elscreen-tab-background-face ((t (:background "dark slate gray"))))
- '(elscreen-tab-current-screen-face ((t (:background "DarkSeaGreen4" :foreground "white"))))
+ '(elscreen-tab-current-screen-face ((t (:background "DarkSlateGray4" :foreground "white"))))
  '(elscreen-tab-other-screen-face ((t (:background "dark slate gray" :foreground "Gray50"))))
  '(helm-candidate-number ((t (:background "#1c1f26" :foreground "#ffffff"))))
  '(variable-pitch ((t (:family "Ricty")))))
