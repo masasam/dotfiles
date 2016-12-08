8d83bd0d834234ae501a272a8c1925fb3346827e
masa <masasam@users.noreply.github.com>
2016-12-08 18:49:38 +0900

    elscreenをカスタマイズした

--- .emacs.d/inits/37elscreen.el
+++ .emacs.d/inits/37elscreen.el
@@ -1,7 +1,9 @@
-;;; プレフィクスキーはC-z
+;;; プレフィクスキー
 (setq elscreen-prefix-key (kbd "C-z"))
 (elscreen-start)
 ;;; タブの先頭に[X]を表示しない
 (setq elscreen-tab-display-kill-screen nil)
 ;;; header-lineの先頭に[<->]を表示しない
 (setq elscreen-tab-display-control nil)
+;; Don't show tab number in mode-line
+(setq elscreen-display-screen-number nil)
--- .emacs.d/inits/99_custom.el
+++ .emacs.d/inits/99_custom.el
@@ -4,8 +4,8 @@
 ;;;; themeの一部分を自分好みに変更 M-x list-faces-display で調べて変更する
 (custom-set-faces
  '(fixed-pitch ((t (:family "Ricty"))))
- '(elscreen-tab-background-face ((t (:background "dark slate gray"))))
- '(elscreen-tab-current-screen-face ((t (:background "DarkSlateGray4" :foreground "white"))))
- '(elscreen-tab-other-screen-face ((t (:background "dark slate gray" :foreground "Gray50"))))
+ '(elscreen-tab-background-face ((t (:background "#37474f"))))
+ '(elscreen-tab-current-screen-face ((t (:background "dark slate gray" :foreground "white"))))
+ '(elscreen-tab-other-screen-face ((t (:background "#37474f" :foreground "Gray50"))))
  '(helm-candidate-number ((t (:background "#1c1f26" :foreground "#ffffff"))))
  '(variable-pitch ((t (:family "Ricty")))))
