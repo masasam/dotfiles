15a0f23510317f6ee73ee31ee032ed66549ca665
masa <masasam@users.noreply.github.com>
2016-12-08 17:58:06 +0900

    elscreenをインストールした

---
+++ .emacs.d/inits/37elscreen.el
@@ -0,0 +1,7 @@
+;;; プレフィクスキーはC-z
+(setq elscreen-prefix-key (kbd "C-z"))
+(elscreen-start)
+;;; タブの先頭に[X]を表示しない
+(setq elscreen-tab-display-kill-screen nil)
+;;; header-lineの先頭に[<->]を表示しない
+(setq elscreen-tab-display-control nil)
