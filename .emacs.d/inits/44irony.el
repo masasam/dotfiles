79de1af44c6b065ce707d0ce5d730c245e541147
masa <masasam@users.noreply.github.com>
2016-12-29 11:33:38 +0900

    ironyでcの環境を整えた

---
+++ .emacs.d/inits/44irony.el
@@ -0,0 +1,6 @@
+(eval-after-load "irony"
+  '(progn
+     (custom-set-variables '(irony-additional-clang-options '("-std=c++11")))
+     (add-to-list 'company-backends 'company-irony)
+     (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+     (add-hook 'c-mode-common-hook 'irony-mode)))
