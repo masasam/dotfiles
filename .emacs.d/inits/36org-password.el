41e055a0ed609139be778dfa721a28b85ab20cf7
masa <masasam@users.noreply.github.com>
2017-01-10 14:13:01 +0900

    org-passwordからenpassに乗り換え

--- .emacs.d/inits/36org-password.el
+++
@@ -1,9 +0,0 @@
-(setq epa-file-cache-passphrase-for-symmetric-encryption t)
-
-(require 'org-agenda)
-(push "~/Dropbox/passwd/password.org.gpg" org-agenda-files)
-
-;; password登録
-(defun addpassword ()
-  (interactive)
-  (find-file "~/Dropbox/passwd/password.org.gpg"))
