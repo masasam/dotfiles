c37c0f8d07171f7e101814342e1be6e9c3f1e0dd
masa <masasam@users.noreply.github.com>
2017-01-12 17:00:59 +0900

    emacsとgtk+のバージョンを出すようにした

--- .emacs.d/inits/000.dashboard.el
+++ .emacs.d/inits/000.dashboard.el
@@ -3,7 +3,7 @@
 ;; 起動画面をdashboardで変更
 (require 'dashboard)
 ;; Set the title
-(setq dashboard-banner-logo-title emacs-version)
+(setq dashboard-banner-logo-title (concat "GNU Emacs " emacs-version " x86_64-Archlinux-gnu GTK+ Version " gtk-version-string))
 ;; Set the banner
 (setq dashboard-startup-banner "~/Dropbox/emacs/emacs.png")
 (dashboard-setup-startup-hook)
