a787a8d3a35e9b3b462a7dfff6ca96fe01c5244d
masa <masasam@users.noreply.github.com>
2017-01-19 03:10:50 +0900

    ironyとansibleで使うcompany-modeの設定をした

--- .emacs.d/inits/35company.el
+++ .emacs.d/inits/35company.el
@@ -1,38 +1,26 @@
-(global-company-mode +1)
+(require 'company)
+(add-hook 'yaml-mode-hook
+          (lambda ()
+            (company-mode 1)))
+(add-hook 'yaml-mode-hook
+          (lambda ()
+            (add-to-list 'company-backends 'company-ansible)))
+(add-hook 'irony-mode-hook
+          (lambda ()
+            (company-mode 1)))
+(add-hook 'irony-mode-hook
+          (lambda ()
+            (add-to-list 'company-backends 'company-irony)))
 
-;; 自動補完を offにしたい場合は, company-idle-delayを nilに設定する
-;; auto-completeでいうところの ac-auto-start にあたる.
-(custom-set-variables
- '(company-idle-delay nil))
-
-(set-face-attribute 'company-tooltip nil
-                    :foreground "black" :background "lightgrey")
-(set-face-attribute 'company-tooltip-common nil
-                    :foreground "black" :background "lightgrey")
-(set-face-attribute 'company-tooltip-common-selection nil
-                    :foreground "white" :background "steelblue")
-(set-face-attribute 'company-tooltip-selection nil
-                    :foreground "black" :background "steelblue")
-(set-face-attribute 'company-preview-common nil
-                    :background nil :foreground "lightgrey" :underline t)
-(set-face-attribute 'company-scrollbar-fg nil
-                    :background "blue")
-(set-face-attribute 'company-scrollbar-bg nil
-                    :background "gray40")
-
+;; general
+(setq comany-idle-delay 0.1)
+(setq comany-minimum-prefix-length 2)
+(setq comany-selection-wrap-around t)
+;; (setq company-idle-delay nil) ; 自動補完をしない
 (global-set-key (kbd "C-M-i") 'company-complete)
-
-;; C-n, C-pで補完候補を次/前の候補を選択
+(define-key company-active-map (kbd "C-h") nil)
 (define-key company-active-map (kbd "C-n") 'company-select-next)
 (define-key company-active-map (kbd "C-p") 'company-select-previous)
 (define-key company-search-map (kbd "C-n") 'company-select-next)
 (define-key company-search-map (kbd "C-p") 'company-select-previous)
-
-;; C-sで絞り込む
-(define-key company-active-map (kbd "C-s") 'company-filter-candidates)
-
-;; TABで候補を設定
-(define-key company-active-map (kbd "C-i") 'company-complete-selection)
-
-;; 各種メジャーモードでも C-M-iで company-modeの補完を使う
-(define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
+(define-key company-active-map (kbd "<tab>") 'company-complete-selection)
