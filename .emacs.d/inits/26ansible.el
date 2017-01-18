caeb88e7ad5467c64855593597f0a773f2a42381
masa <masasam@users.noreply.github.com>
2017-01-18 22:00:53 +0900

    ansible-doc-modeをインストール

--- .emacs.d/inits/26ansible.el
+++ .emacs.d/inits/26ansible.el
@@ -2,3 +2,6 @@
 
 (setq ansible::vault-password-file "~/Dropbox/ansible/vault_pass")
 (add-hook 'ansible-hook 'ansible::auto-decrypt-encrypt)
+
+;; ansible-doc
+(add-hook 'yaml-mode-hook #'ansible-doc-mode)
