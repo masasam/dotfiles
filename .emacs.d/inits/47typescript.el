33f98d304744517005ff90f06bcf2bc2f5b68e1b
masa <masasam@users.noreply.github.com>
2017-01-19 13:48:56 +0900

    emacsでtypescriptの環境を整えた

---
+++ .emacs.d/inits/47typescript.el
@@ -0,0 +1,8 @@
+(require 'tide)
+(add-hook 'typescript-mode-hook
+          (lambda ()
+            (tide-setup)
+            (flycheck-mode t)
+            (setq flycheck-check-syntax-automatically '(save mode-enabled))
+            (eldoc-mode t)
+            (company-mode-on)))
