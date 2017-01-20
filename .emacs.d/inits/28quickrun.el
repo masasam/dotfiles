b41e1cf4ca75263035e9aa80ced03ab623f2fe1e
masa <masasam@users.noreply.github.com>
2017-01-20 13:25:33 +0900

    quickrunでリージョン選択してるときの判定をいれた

--- .emacs.d/inits/28quickrun.el
+++ .emacs.d/inits/28quickrun.el
@@ -1,1 +1,9 @@
 (require 'quickrun)
+(push '("*quickrun*") popwin:special-display-config)
+(global-set-key (kbd "<f5>") 'quickrun-sc)
+
+(defun quickrun-sc (start end)
+(interactive "r")
+(if mark-active
+    (quickrun :start start :end end)
+  (quickrun)))
