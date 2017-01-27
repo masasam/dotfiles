9fa138b1a152f6c0554e0b5bdf608069e2487601
masa <masasam@users.noreply.github.com>
2017-01-27 13:05:43 +0900

    Change quickrun setting

--- .emacs.d/inits/28quickrun.el
+++ .emacs.d/inits/28quickrun.el
@@ -1,9 +1,3 @@
 (require 'quickrun)
 (push '("*quickrun*") popwin:special-display-config)
-(global-set-key (kbd "<f5>") 'quickrun-sc)
-
-(defun quickrun-sc (start end)
-(interactive "r")
-(if mark-active
-    (quickrun :start start :end end)
-  (quickrun)))
+(global-set-key (kbd "<f5>") 'quickrun)
