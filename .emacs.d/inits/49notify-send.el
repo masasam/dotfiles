70eb33f1c518abba7e210dfc43ffa692967bd474
masa <masasam@users.noreply.github.com>
2017-02-01 12:03:56 +0900

    Remove unnecessary elisp

--- .emacs.d/inits/49notify-send.el
+++
@@ -1,5 +0,0 @@
-(defun notify-send-after-save-hook ()
-  (shell-command
-   (format "notify-send -i /usr/share/icons/hicolor/24x24/apps/emacs.png \"%s を保存しました\""
-	   (buffer-name (current-buffer)))))
-(add-hook 'after-save-hook 'notify-send-after-save-hook)
