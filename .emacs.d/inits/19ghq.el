a9afdde95fc71e18784ae0cb321b8b2c8c973d90
masa <masasam@users.noreply.github.com>
2017-02-02 12:01:43 +0900

    Replace keybind of helm-ghq with bindkey

--- .emacs.d/inits/19ghq.el
+++ .emacs.d/inits/19ghq.el
@@ -1,4 +1,4 @@
 (add-to-list 'helm-for-files-preferred-list 'helm-source-ghq)
-;; helm-ghq with (C-x l , C-x C-l)
-(global-set-key (kbd "C-x l") 'helm-ghq)
-(global-set-key (kbd "C-x C-l") 'helm-ghq)
+(require 'bind-key)
+(bind-key "C-x l" 'helm-ghq)
+(bind-key "C-x C-l" 'helm-ghq)
