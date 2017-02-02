8978e492a7c660749b2e188555df0f6ab40f2d19
masa <masasam@users.noreply.github.com>
2017-02-02 11:55:27 +0900

    Replace keybind of dired-jump with bindkey

--- .emacs.d/inits/16direx.el
+++ .emacs.d/inits/16direx.el
@@ -15,8 +15,9 @@
                (direx-project:jump-to-project-root-other-window) t)
              (direx:jump-to-directory-other-window)))))
 
-(global-set-key (kbd "C-x C-j") 'my/dired-jump)
-(global-set-key (kbd "C-x j") 'my/dired-jump)
+(require 'bind-key)
+(bind-key "C-x C-j" 'my/dired-jump)
+(bind-key "C-x j" 'my/dired-jump)
 
 ;; Adjust width according to the environment.
 (push '(direx:direx-mode :position left :width 40 :dedicated t)
