e2c57968a05f862591bf315479764e0f7d20aca1
masa <masasam@users.noreply.github.com>
2017-02-16 16:45:34 +0900

    Connect tramp with bash

--- .emacs.d/inits/31tramp.el
+++ .emacs.d/inits/31tramp.el
@@ -5,3 +5,6 @@
              '("localhost" nil nil))
 (add-to-list 'tramp-default-proxies-alist
              '((regexp-quote (system-name)) nil nil))
+
+;; Connect tramp with bash
+(eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
