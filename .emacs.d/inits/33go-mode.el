bd9bbfb36d4cdfb6d74bc80ba498302fa4858508
masa <masasam@users.noreply.github.com>
2017-02-21 10:23:53 +0900

    Add helm-go-package

--- .emacs.d/inits/33go-mode.el
+++ .emacs.d/inits/33go-mode.el
@@ -1,22 +1,23 @@
+;; go-mode
+(require 'go-eldoc)
 (require 'go-autocomplete)
 (require 'auto-complete-config)
-;; go-mode
 (setenv "GOPATH" "/home/masa")
 (add-to-list 'exec-path (expand-file-name "~/bin"))
 
-;; eldoc
-(require 'go-eldoc)
-(add-hook 'go-mode-hook 'go-eldoc-setup)
-
-;; gofmt goimports
-(setq gofmt-command "goimports")
-(add-hook 'before-save-hook 'gofmt-before-save)
-
 (add-hook 'go-mode-hook
           '(lambda()
+	     (setq gofmt-command "goimports")
+	     (add-hook 'before-save-hook 'gofmt-before-save)
              (setq c-basic-offset 4)
              (setq indent-tabs-mode t)
              (local-set-key (kbd "M-.") 'godef-jump)
              (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
              (local-set-key (kbd "C-c a") 'go-import-add)
-             (local-set-key (kbd "C-c d") 'godoc)))
+             (local-set-key (kbd "C-c d") 'godoc)
+	     (go-eldoc-setup)))
+
+(require 'helm-go-package)
+(autoload 'helm-go-package "helm-go-package")
+(eval-after-load 'go-mode
+  '(substitute-key-definition 'go-import-add 'helm-go-package go-mode-map))
