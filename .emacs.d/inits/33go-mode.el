b14d9b52d66598d12f3fc04ddc021c118322a676
masa <masasam@users.noreply.github.com>
2017-02-21 10:39:09 +0900

    Change configuration at go-mode

--- .emacs.d/inits/33go-mode.el
+++ .emacs.d/inits/33go-mode.el
@@ -12,9 +12,6 @@
              (setq c-basic-offset 4)
              (setq indent-tabs-mode t)
              (local-set-key (kbd "M-.") 'godef-jump)
-             (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
-             (local-set-key (kbd "C-c a") 'go-import-add)
-             (local-set-key (kbd "C-c d") 'godoc)
 	     (go-eldoc-setup)))
 
 (require 'helm-go-package)
