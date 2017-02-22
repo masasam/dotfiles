2b46a0a3f7cc62768f3610c84ea3ac1ace39b724
masa <masasam@users.noreply.github.com>
2017-02-23 08:00:06 +0900

    Change cursor color

--- .emacs.d/inits/99_custom.el
+++ .emacs.d/inits/99_custom.el
@@ -4,6 +4,7 @@
 ;; Investigate by changing 'M-x list-faces-display'
 (custom-set-faces
  '(fixed-pitch ((t (:family "Ricty"))))
+ '(cursor ((t (:background "#82c600"))))
  '(elscreen-tab-background-face ((t (:background "#37474f"))))
  '(elscreen-tab-current-screen-face ((t (:background "LightSkyBlue4" :foreground "white"))))
  '(elscreen-tab-other-screen-face ((t (:background "#37474f" :foreground "Gray50"))))
