ef45a56ea9217557386fefb5ccdf83fd4dc8cffc
masa <masasam@users.noreply.github.com>
2017-02-22 15:43:19 +0900

    Fix bug helm-gtags

--- .emacs.d/inits/04helm.el
+++ .emacs.d/inits/04helm.el
@@ -84,16 +84,14 @@
  '(helm-gtags-auto-update t))
 
 ;; key bindings
-(eval-after-load "helm-gtags"
-  '(progn
-     (bind-key "M-t" 'helm-gtags-find-tag helm-gtags-mode-map)
-     (bind-key "M-r" 'helm-gtags-find-rtag helm-gtags-mode-define)
-     (bind-key "M-s" 'helm-gtags-find-symbol helm-gtags-mode-map)
-     (bind-key "M-g M-p" 'helm-gtags-parse-file helm-gtags-mode-map)
-     (bind-key "C-c <" 'helm-gtags-previous-history helm-gtags-mode-map)
-     (bind-key "C-c >" 'helm-gtags-next-history helm-gtags-mode-map)
-     (bind-key "M-," 'helm-gtags-pop-stack helm-gtags-mode-map)))
-
+(with-eval-after-load 'helm-gtags
+  (define-key helm-gtags-mode-map (kbd "M-t") 'helm-gtags-find-tag)
+  (define-key helm-gtags-mode-map (kbd "M-r") 'helm-gtags-find-rtag)
+  (define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-find-symbol)
+  (define-key helm-gtags-mode-map (kbd "M-g M-p") 'helm-gtags-parse-file)
+  (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+  (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+  (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack))
 
 ;; helm-projectile
 (require 'helm-projectile)
