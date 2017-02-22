6163586d397e1acfde0f913414ea2fb884c0a98e
masa <masasam@users.noreply.github.com>
2017-02-22 18:28:10 +0900

    Delete ctags-update

--- .emacs.d/inits/21ctags-update.el
+++
@@ -1,5 +0,0 @@
-(setq ctags-update-command "/usr/bin/ctags")
-;; if you want to update (create) TAGS manually
-(autoload 'ctags-update "ctags-update" "update TAGS using ctags" t)
-(require 'bind-key)
-(bind-key "C-c E" 'ctags-update)
