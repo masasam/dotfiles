46e48002d158c363bedb8401b6374669fe7809bc
masa <masasam@users.noreply.github.com>
2017-02-22 18:45:47 +0900

    Delete ac-etags

--- .emacs.d/inits/29ac-etags.el
+++
@@ -1,9 +0,0 @@
-(custom-set-variables
-  '(ac-etags-requires 1))
-
-(eval-after-load "etags"
-  '(progn
-      (ac-etags-setup)))
-
-(add-hook 'c-mode-common-hook 'ac-etags-ac-setup)
-(add-hook 'ruby-mode-common-hook 'ac-etags-ac-setup)
