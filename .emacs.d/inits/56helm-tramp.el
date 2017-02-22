e4f122f4129c558b83f040777d5ba7d868f4cd60
masa <masasam@users.noreply.github.com>
2017-02-22 18:48:43 +0900

    Organize tramp configuration file

--- .emacs.d/inits/56helm-tramp.el
+++ .emacs.d/inits/56helm-tramp.el
@@ -2,3 +2,5 @@
 (defalias 'exit-tramp 'tramp-cleanup-all-buffers)
 (require 'bind-key)
 (bind-key "C-c s" 'helm-tramp)
+;; Connect tramp with bash
+(eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
