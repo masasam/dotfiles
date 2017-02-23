567a2f04d4bbbbed271ba5818846e1ad1ed8ba87
masa <masasam@users.noreply.github.com>
2017-02-23 11:24:54 +0900

    Disable dashboard with emacs -nw

--- .emacs.d/inits/01.dashboard.el
+++ .emacs.d/inits/01.dashboard.el
@@ -1,10 +1,11 @@
-;; Hide splash at start
-(setq inhibit-startup-message t)
-;; Change start screen with dashboard
-(require 'dashboard)
-;; Set the title
-(setq dashboard-banner-logo-title (concat "GNU Emacs " emacs-version " x86_64-Archlinux-gnu GTK+ Version " gtk-version-string))
-;; Set the banner
-(setq dashboard-startup-banner "~/Dropbox/emacs/emacs.png")
-(dashboard-setup-startup-hook)
-(setq dashboard-items '((recents  . 20)))
+(when (window-system)
+  ;; Hide splash at start
+  (setq inhibit-startup-message t)
+  ;; Change start screen with dashboard
+  (require 'dashboard)
+  ;; Set the title
+  (setq dashboard-banner-logo-title (concat "GNU Emacs " emacs-version " x86_64-Archlinux-gnu GTK+ Version " gtk-version-string))
+  ;; Set the banner
+  (setq dashboard-startup-banner "~/Dropbox/emacs/emacs.png")
+  (dashboard-setup-startup-hook)
+  (setq dashboard-items '((recents  . 20))))
