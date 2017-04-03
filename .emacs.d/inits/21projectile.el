41dc331413816a05c2ed07d9ddb1db5719cca562
Masashi Miyaura <masasam@users.noreply.github.com>
2017-04-03 09:18:54 +0900

    Update projectile

--- .emacs.d/inits/21projectile.el
+++ .emacs.d/inits/21projectile.el
@@ -1,3 +1,21 @@
 (projectile-global-mode)
 (setq projectile-completion-system 'helm)
+(require 'helm-projectile)
 (helm-projectile-on)
+
+
+(progn
+  (custom-set-variables
+   '(helm-truncate-lines t)
+   '(helm-delete-minibuffer-contents-from-point t)
+   '(helm-mini-default-sources '(helm-source-buffers-list
+                                 helm-source-files-in-current-dir
+                                 helm-source-recentf
+				 helm-source-projectile-files-list
+                                 ))))
+
+
+;; helm-projectile
+(setq helm-projectile-fuzzy-match nil)
+;; How to clear cache (M-x projectile-invalidate-cache)
+(setq projectile-enable-caching t)
