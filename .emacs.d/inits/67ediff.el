0d6a67abe38414bcecc4f2914d03a5dfca77a01c
Masashi Miyaura <masasam@users.noreply.github.com>
2017-04-03 09:53:31 +0900

    Add ediff

---
+++ .emacs.d/inits/67ediff.el
@@ -0,0 +1,5 @@
+;; ediff
+(custom-set-variables
+ '(ediff-window-setup-function 'ediff-setup-windows-plain)
+ '(ediff-split-window-function 'split-window-horizontally)
+ '(ediff-diff-options "-twB"))
