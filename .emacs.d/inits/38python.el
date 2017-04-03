f784303876bff33eed7c5daf5b1389ade4cb3e51
Masashi Miyaura <masasam@users.noreply.github.com>
2017-04-03 10:18:59 +0900

    Add virtualenv

--- .emacs.d/inits/38python.el
+++ .emacs.d/inits/38python.el
@@ -5,3 +5,7 @@
 (setq py-autopep8-options '("--max-line-length=200"))
 (setq flycheck-flake8-maximum-line-length 200)
 (py-autopep8-enable-on-save)
+
+(require 'virtualenvwrapper)
+(require 'auto-virtualenvwrapper)
+(add-hook 'python-mode-hook #'auto-virtualenvwrapper-activate)
