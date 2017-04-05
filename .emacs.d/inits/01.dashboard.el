6b4c7267875722ae27bb3dd1c42bdaec3344dbab
Masashi Miyaura <masasam@users.noreply.github.com>
2017-04-06 07:38:37 +0900

    Change dashboard

--- .emacs.d/inits/01.dashboard.el
+++ .emacs.d/inits/01.dashboard.el
@@ -4,8 +4,8 @@
   ;; Change start screen with dashboard
   (require 'dashboard)
   ;; Set the title
-  (setq dashboard-banner-logo-title (concat "GNU Emacs " emacs-version " kernel " (car (split-string (shell-command-to-string "uname -r") "-"))  " x86_64-Archlinux GTK+ " gtk-version-string))
+  (setq dashboard-banner-logo-title (concat "GNU Emacs " emacs-version " kernel " (car (split-string (shell-command-to-string "uname -r") "-"))  " x86_64 ArchLinux GTK+ " gtk-version-string))
   ;; Set the banner
   (setq dashboard-startup-banner "~/Dropbox/emacs/emacs.png")
   (dashboard-setup-startup-hook)
-  (setq dashboard-items '((recents  . 20))))
+  (setq dashboard-items '((recents  . 15))))
