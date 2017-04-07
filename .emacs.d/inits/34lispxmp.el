008aa29bafd30f0a31cdd648d3ac62fd07e92b33
Masashi Miyaura <masasam@users.noreply.github.com>
2017-04-08 05:54:47 +0900

    Delete lispxmp

--- .emacs.d/inits/34lispxmp.el
+++
@@ -1,5 +0,0 @@
-;; Setting to annotate the evaluation result of the expression
-(require 'lispxmp)
-;; Annotated by pressing C-c C-d in emacs-lisp-mode
-(require 'bind-key)
-(bind-key "C-c C-d" 'lispxmp emacs-lisp-mode-map)
