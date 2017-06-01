c8efb02f6fa64e917682d25c5b37293db7a3de00
Masashi Miyaura <masasam@users.noreply.github.com>
2017-06-01 11:17:08 +0900

    Add ac-php

--- .emacs.d/inits/58php-mode.el
+++ .emacs.d/inits/58php-mode.el
@@ -1,3 +1,14 @@
+(require 'cl)
 (require 'php-mode)
-(setq php-mode-force-pear t)
-(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-hook 'php-mode-hook
+	  '(lambda ()
+	     (auto-complete-mode t)
+	     (require 'ac-php)
+	     (setq ac-sources  '(ac-source-php ) )
+	     (yas-global-mode 1)
+
+	     (ac-php-core-eldoc-setup ) ;; enable eldoc
+	     (define-key php-mode-map  (kbd "C-]") 'ac-php-find-symbol-at-point)   ;goto define
+	     (define-key php-mode-map  (kbd "C-t") 'ac-php-location-stack-back)    ;go back
+	     ))
+(add-hook 'php-mode-hook 'php-enable-pear-coding-style)
