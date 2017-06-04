cb6220a2eb910d1e47e50155ec0cb0482cbc718a
Masashi Miyaura <masasam@users.noreply.github.com>
2017-06-04 20:37:29 +0900

    Update helm-gtags

--- .emacs.d/inits/64helm-gtags.el
+++ .emacs.d/inits/64helm-gtags.el
@@ -4,12 +4,11 @@
  '(helm-gtags-ignore-case t)
  '(helm-gtags-auto-update t))
 
-;; key bindings
-(with-eval-after-load 'helm-gtags
-  (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-find-tag)
-  (define-key helm-gtags-mode-map (kbd "M-r") 'helm-gtags-find-rtag)
-  (define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-find-symbol)
-  (define-key helm-gtags-mode-map (kbd "M-g M-p") 'helm-gtags-parse-file)
-  (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-  (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
-  (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack))
+(add-hook 'helm-gtags-mode-hook
+	  '(lambda ()
+	     (local-set-key (kbd "M-.") 'helm-gtags-find-tag)
+	     (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
+	     (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
+	     (local-set-key (kbd "M-l") 'helm-gtags-select)
+	     (local-set-key (kbd "M-,") 'helm-gtags-pop-stack)))
+(add-hook 'php-mode-hook 'helm-gtags-mode)
