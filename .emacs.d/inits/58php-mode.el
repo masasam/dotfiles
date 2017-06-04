a06bb4204ca17eb1dfd39b17e7cfdd5ad531d20a
Masashi Miyaura <masasam@users.noreply.github.com>
2017-06-04 20:42:41 +0900

    Fix don't use eldoc-php

--- .emacs.d/inits/58php-mode.el
+++ .emacs.d/inits/58php-mode.el
@@ -1,13 +1,11 @@
-(require 'cl)
 (require 'php-mode)
 (add-hook 'php-mode-hook
 	  '(lambda ()
 	     (auto-complete-mode t)
 	     (require 'ac-php)
-	     (setq ac-sources  '(ac-source-php ) )
+	     (setq ac-sources  '(ac-source-php ))
 	     (yas-global-mode 1)
-
-	     (ac-php-core-eldoc-setup ) ;; enable eldoc
+	     ;;(ac-php-core-eldoc-setup) ;; enable eldoc
 	     (define-key php-mode-map  (kbd "C-]") 'ac-php-find-symbol-at-point)   ;goto define
 	     (define-key php-mode-map  (kbd "C-t") 'ac-php-location-stack-back)    ;go back
 	     ))
