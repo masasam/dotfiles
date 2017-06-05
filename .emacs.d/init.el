17f3d65c7ab551c2c8bd1a7f5002bc3d9aa714fe
Masashi Miyaura <masasam@users.noreply.github.com>
2017-06-06 01:45:29 +0900

    Fix fullscreen bug

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,9 @@
 (package-initialize)
 
 (set-frame-parameter nil 'fullscreen 'maximized)
+(custom-set-variables
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
+
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
