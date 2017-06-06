5bdbd0163dbdcb743eeaca125655b1ea87936ce4
Masashi Miyaura <masasam@users.noreply.github.com>
2017-06-07 06:50:22 +0900

    Add php-eldoc

--- .emacs.d/inits/58php-mode.el
+++ .emacs.d/inits/58php-mode.el
@@ -5,7 +5,8 @@
 	     (require 'ac-php)
 	     (setq ac-sources  '(ac-source-php ))
 	     (yas-global-mode 1)
-	     ;;(ac-php-core-eldoc-setup) ;; enable eldoc
+	     ;;(ac-php-core-eldoc-setup) ;; enable ac-php eldoc
+	     (php-eldoc-enable) ;;php-eldoc
 	     (define-key php-mode-map  (kbd "C-]") 'ac-php-find-symbol-at-point)   ;goto define
 	     (define-key php-mode-map  (kbd "C-t") 'ac-php-location-stack-back)    ;go back
 	     ))
