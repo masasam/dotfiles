ceff4ec0d02204c254718c36f677d022e0202f91
Masashi Miyaura <masasam@users.noreply.github.com>
2017-06-07 10:10:30 +0900

    Make php manual available soon

--- .emacs.d/inits/58php-mode.el
+++ .emacs.d/inits/58php-mode.el
@@ -1,6 +1,7 @@
 (require 'php-mode)
 (add-hook 'php-mode-hook
 	  '(lambda ()
+	     (local-set-key (kbd "<f1>") 'my-php-symbol-lookup)
 	     (auto-complete-mode t)
 	     (require 'ac-php)
 	     (setq ac-sources  '(ac-source-php ))
@@ -11,3 +12,11 @@
 	     (define-key php-mode-map  (kbd "C-t") 'ac-php-location-stack-back)    ;go back
 	     ))
 (add-hook 'php-mode-hook 'php-enable-psr2-coding-style)
+
+(defun my-php-symbol-lookup ()
+  (interactive)
+  (let ((symbol (symbol-at-point)))
+    (if (not symbol)
+        (message "No symbol at point.")
+      (browse-url (concat "http://php.net/manual-lookup.php?pattern="
+                          (symbol-name symbol))))))
