c2afb6d9b2c1d96bdabd8e302750ce034c93075e
Masashi Miyaura <masasam@users.noreply.github.com>
2017-06-22 04:15:26 +0900

    Auto-complete on json-mode and toml-mode

--- .emacs.d/inits/11auto-complete.el
+++ .emacs.d/inits/11auto-complete.el
@@ -7,6 +7,8 @@
 (add-to-list 'ac-modes 'org-mode)
 (add-to-list 'ac-modes 'yatex-mode)
 (add-to-list 'ac-modes 'markdown-mode)
+(add-to-list 'ac-modes 'json-mode)
+(add-to-list 'ac-modes 'toml-mode)
 (ac-set-trigger-key "TAB")
 (setq ac-use-menu-map t);; Complement candidate selection with C-n / C-p when complement menu is displayed
 (setq ac-use-fuzzy t)   ;; Ambiguous match
