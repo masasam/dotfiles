319812c65b554cc2dd6a3dcc6bd16547455902cc
Masashi Miyaura <masasam@users.noreply.github.com>
2017-07-04 11:02:49 +0900

    Rename helm-directory

--- .emacs.d/inits/63helm-directory-find-file.el
+++
@@ -1,3 +0,0 @@
-(require 'bind-key)
-(bind-key "C-c l" 'helm-directory-find-file)
-(bind-key "C-c C-l" 'helm-directory-find-file)
---
+++ .emacs.d/inits/63helm-directory.el
@@ -0,0 +1,3 @@
+(require 'bind-key)
+(bind-key "C-c l" 'helm-directory)
+(bind-key "C-c C-l" 'helm-directory)
