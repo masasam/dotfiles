8a0c0ab87a38568a393d364c5620bf210dc8c6a0
Masashi Miyaura <masasam@users.noreply.github.com>
2017-09-23 18:15:17 +0900

    Add easy-jekyll

---
+++ .emacs.d/inits/73easy-jekyll.el
@@ -0,0 +1,6 @@
+(setq easy-jekyll-basedir "~/src/github.com/masasam/jekyll/")
+(setq easy-jekyll-url "https://example.org")
+(setq easy-jekyll-sshdomain "blogdomain")
+(setq easy-jekyll-root "/home/jekyll/")
+(setq easy-jekyll-previewtime "300")
+(define-key global-map (kbd "C-c C-j") 'easy-jekyll)
