0f80767bd11ae15324bf0e63ccf8bc93b3cc4a83
Masashi Miyaura <masasam@users.noreply.github.com>
2017-10-08 15:28:12 +0900

    Change .emacs

--- .emacs.d/inits/73easy-jekyll.el
+++ .emacs.d/inits/73easy-jekyll.el
@@ -4,3 +4,14 @@
 (setq easy-jekyll-root "/home/jekyll/")
 (setq easy-jekyll-previewtime "300")
 (define-key global-map (kbd "C-c C-j") 'easy-jekyll)
+;; Total number of blogs
+(setq easy-jekyll-blog-number 3)
+;; Blog1
+(setq easy-jekyll-basedir-1 "~/src/github.com/masasam/jekyll1/")
+(setq easy-jekyll-url-1 "http://example1.com")
+(setq easy-jekyll-root-1 "/home/blog/")
+(setq easy-jekyll-sshdomain-1 "blog1domain")
+;; Blog2
+(setq easy-jekyll-basedir-2 "~/src/github.com/masasam/jekyll2/")
+(setq easy-jekyll-url-2 "http://example2.com")
+(setq easy-jekyll-amazon-s3-bucket-name-2 "blogbucket")
