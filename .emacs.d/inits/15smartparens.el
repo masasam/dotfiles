e216bbf115881e5e21ae4f330832c22fa4eb872d
Masashi Miyaura <masasam@users.noreply.github.com>
2017-11-08 11:07:01 +0900

    Add emacs setting

--- .emacs.d/inits/15smartparens.el
+++ .emacs.d/inits/15smartparens.el
@@ -6,14 +6,4 @@
 (add-hook 'ielm-mode-hook 'turn-off-smartparens-mode)
 (add-hook 'scheme-mode-hook 'turn-off-smartparens-mode)
 (add-hook 'slime-repl-mode-hook 'turn-off-smartparens-mode)
-(require 'paredit)
-(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
-(add-hook 'lisp-mode-hook 'enable-paredit-mode)
-(add-hook 'ielm-mode-hook 'enable-paredit-mode)
-(add-hook 'scheme-mode-hook 'enable-paredit-mode)
-(add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
-(defun override-slime-repl-bindings-with-paredit ()
-  (define-key slime-repl-mode-map
-    (read-kbd-macro paredit-backward-delete-key) nil))
-(add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
+
---
+++ .emacs.d/inits/16paredit.el
@@ -0,0 +1,11 @@
+(require 'paredit)
+(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'ielm-mode-hook 'enable-paredit-mode)
+(add-hook 'scheme-mode-hook 'enable-paredit-mode)
+(add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
+(defun override-slime-repl-bindings-with-paredit ()
+  (define-key slime-repl-mode-map
+    (read-kbd-macro paredit-backward-delete-key) nil))
+(add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
