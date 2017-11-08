f273383fe74768f494b955ba669d8e146ede5672
Masashi Miyaura <masasam@users.noreply.github.com>
2017-11-08 21:18:31 +0900

    Add lua

--- .emacs.d/inits/39javascript.el
+++ .emacs.d/inits/39javascript.el
@@ -1,6 +1,7 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\.jsx$" . js2-jsx-mode))
+(flycheck-add-mode 'javascript-eslint 'js2-jsx-mode)
 
 (add-hook 'js2-mode-hook
           (lambda ()
