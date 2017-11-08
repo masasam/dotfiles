f14ded73d057ef7d10fccbfda3e6c1296ffa97d9
Masashi Miyaura <masasam@users.noreply.github.com>
2017-11-08 09:03:20 +0900

    Change helm-ag

--- .emacs.d/inits/34helm-ag.el
+++ .emacs.d/inits/34helm-ag.el
@@ -4,7 +4,6 @@
 (require 'bind-key)
 (bind-key "M-g ." 'helm-ag)
 (bind-key "M-g ," 'helm-ag-pop-stack)
-(bind-key "C-M-s" 'helm-ag-this-file)
 (custom-set-variables
  '(helm-ag-base-command "ag --nocolor --nogroup --ignore-case")
  '(helm-ag-command-option "--all-text")
