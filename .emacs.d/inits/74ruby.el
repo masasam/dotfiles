48a0f7a0f48bd3888a844fe245063c16296c4d3e
Masashi Miyaura <masasam@users.noreply.github.com>
2017-11-10 15:04:00 +0900

    Add inf-ruby

--- .emacs.d/inits/74ruby.el
+++ .emacs.d/inits/74ruby.el
@@ -2,6 +2,7 @@
              '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
 
 (add-hook 'enh-ruby-mode-hook 'robe-mode)
+(add-hook 'enh-ruby-mode-hook 'inf-ruby-minor-mode)
 
 ;; robe
 (autoload 'robe-mode "robe" "Code navigation, documentation lookup and completion for Ruby" t nil)
