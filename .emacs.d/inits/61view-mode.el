6a402dd9496b293d4ebbbd219b6a9d33058fe883
Masashi Miyaura <masasam@users.noreply.github.com>
2017-11-17 12:13:19 +0900

    Change view-mode setting

--- .emacs.d/inits/61view-mode.el
+++ .emacs.d/inits/61view-mode.el
@@ -6,14 +6,13 @@
 ;; view-mode
 (setq view-read-only t)
 (require 'view)
-;; less like
 (require 'bind-key)
 (bind-key "N" 'View-search-last-regexp-backward view-mode-map)
 (bind-key "?" 'View-search-regexp-backward view-mode-map)
 (bind-key "G" 'View-goto-line-last view-mode-map)
 (bind-key "b" 'View-scroll-page-backward view-mode-map)
 (bind-key "f" 'View-scroll-page-forward view-mode-map)
-;; vi/w3m like
+(bind-key "i" 'read-only-mode view-mode-map)
 (bind-key "h" 'backward-char view-mode-map)
 (bind-key "j" 'next-line view-mode-map)
 (bind-key "k" 'previous-line view-mode-map)
