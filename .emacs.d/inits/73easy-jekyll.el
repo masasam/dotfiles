354b69d9e55e9e43ea3acc4bb7382a73df43f635
Masashi Miyaura <masasam@users.noreply.github.com>
2017-11-20 19:34:53 +0900

    Add easy-jekyll setting

--- .emacs.d/inits/73easy-jekyll.el
+++ .emacs.d/inits/73easy-jekyll.el
@@ -4,14 +4,10 @@
 (setq easy-jekyll-root "/home/jekyll/")
 (setq easy-jekyll-previewtime "300")
 (define-key global-map (kbd "C-c C-j") 'easy-jekyll)
-;; Total number of blogs
-(setq easy-jekyll-blog-number 3)
-;; Blog1
-(setq easy-jekyll-basedir-1 "~/src/github.com/masasam/jekyll1/")
-(setq easy-jekyll-url-1 "http://example1.com")
-(setq easy-jekyll-root-1 "/home/blog/")
-(setq easy-jekyll-sshdomain-1 "blog1domain")
-;; Blog2
-(setq easy-jekyll-basedir-2 "~/src/github.com/masasam/jekyll2/")
-(setq easy-jekyll-url-2 "http://example2.com")
-(setq easy-jekyll-amazon-s3-bucket-name-2 "blogbucket")
+
+(setq easy-jekyll-bloglist '(((easy-jekyll-basedir . "~/src/github.com/masasam/jekyll1/")
+			      (easy-jekyll-url . "http://example1.com")
+			      (easy-jekyll-sshdomain . "blogdomain")
+			      (easy-jekyll-root . "/home/jekyll"))
+			     ((easy-jekyll-basedir . "~/src/github.com/masasam/jekyll2/")
+			      (easy-jekyll-url . "http://example2.net"))))
