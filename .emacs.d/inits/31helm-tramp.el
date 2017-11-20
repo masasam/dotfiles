19973242a4efb8ddf0f8c219fbcff4f3d96a4de6
Masashi Miyaura <masasam@users.noreply.github.com>
2017-11-20 19:34:11 +0900

    Change helm-tramp setting

--- .emacs.d/inits/31helm-tramp.el
+++ .emacs.d/inits/31helm-tramp.el
@@ -4,4 +4,5 @@
 (bind-key "C-c s" 'helm-tramp)
 ;; Connect tramp with bash
 (eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
-(setq helm-tramp-docker-user "nginx")
+;;(setq helm-tramp-docker-user "nginx")
+(setq helm-tramp-docker-user '("admin" "user" "masa"))
