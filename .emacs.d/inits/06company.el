763a605fc7fd9c91849eb7ea4cbf6d18045eb694
Masashi Miyaura <masasam@users.noreply.github.com>
2017-11-24 20:59:17 +0900

    Fix irony

--- .emacs.d/inits/06company.el
+++ .emacs.d/inits/06company.el
@@ -1,16 +1,4 @@
 (require 'company)
-(add-hook 'yaml-mode-hook
-          (lambda ()
-            (company-mode 1)))
-(add-hook 'yaml-mode-hook
-          (lambda ()
-            (add-to-list 'company-backends 'company-ansible)))
-(add-hook 'irony-mode-hook
-          (lambda ()
-            (company-mode 1)))
-(add-hook 'irony-mode-hook
-          (lambda ()
-            (add-to-list 'company-backends 'company-irony)))
 
 ;; general
 (setq comany-idle-delay 0.1)
--- .emacs.d/inits/26ansible.el
+++ .emacs.d/inits/26ansible.el
@@ -1,4 +1,9 @@
-(add-hook 'yaml-mode-hook '(lambda () (ansible 1)))
+(add-hook 'yaml-mode-hook
+          (lambda ()
+	    (ansible 1)
+            (company-mode 1)
+	    (add-to-list 'company-backends 'company-ansible)))
+
 (setq ansible::vault-password-file "~/Dropbox/ansible/vault_pass")
 (add-hook 'ansible-hook 'ansible::auto-decrypt-encrypt)
 
