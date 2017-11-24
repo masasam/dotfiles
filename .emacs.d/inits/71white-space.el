8e2e7cacd7bd27d2250b74c26a4c8a68c290702e
Masashi Miyaura <masasam@users.noreply.github.com>
2017-11-24 14:46:44 +0900

    Add function in relation to whitespace

---
+++ .emacs.d/inits/71white-space.el
@@ -0,0 +1,20 @@
+;; Highlight the space at the end of the line
+(setq-default show-trailing-whitespace t)
+
+(defun my/disable-trailing-mode ()
+  "Disable show tail whitespace."
+  (setq show-trailing-whitespace nil))
+
+(add-hook 'term-mode-hook 'my/disable-trailing-mode)
+(add-hook 'eshell-mode-hook 'my/disable-trailing-mode)
+
+
+;; Remove contiguous line breaks at end of line + end of file
+(defun my/cleanup-for-spaces ()
+  (interactive)
+  (delete-trailing-whitespace)
+  (save-excursion
+    (save-restriction
+      (widen)
+      (goto-char (point-max))
+      (delete-blank-lines))))
