8bf618363c2dbed12a4d21af3a64c31a88627bfb
Masashi Miyaura <masasam@users.noreply.github.com>
2017-11-24 17:25:47 +0900

    Integrate

--- .emacs.d/inits/06org.el
+++ .emacs.d/inits/06org.el
@@ -61,3 +61,19 @@
     ("C-b" . org-backward-heading-same-level)))
 
 (setq org-src-fontify-natively t)
+
+(with-eval-after-load "calendar"
+  (require 'japanese-holidays)
+  (setq calendar-holidays ;Adjust appropriately if you want to display holidays in other countries as well
+        (append japanese-holidays holiday-local-holidays holiday-other-holidays))
+  (setq calendar-mark-holidays-flag t)    ;Display holidays on the calendar
+  ;; To display Saturdays and Sundays as a holiday, add the following settings.
+  (setq japanese-holiday-weekend '(0 6)    ;Saturday and Sunday as a holiday
+        japanese-holiday-weekend-marker    ;Saturday in light blue
+        '(holiday nil nil nil nil nil japanese-holiday-saturday))
+  (add-hook 'calendar-today-visible-hook 'japanese-holiday-mark-weekend)
+  (add-hook 'calendar-today-invisible-hook 'japanese-holiday-mark-weekend)
+  ;; To mark "Today", add the following settings.
+  (add-hook 'calendar-today-visible-hook 'calendar-mark-today)
+  ;; Display holidays with org-agenda
+  (setq org-agenda-include-diary t))
