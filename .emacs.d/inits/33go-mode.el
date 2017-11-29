cf40af998aa93fd36fd4355612da783c675620ac
Masashi Miyaura <masasam@users.noreply.github.com>
2017-11-29 16:11:37 +0900

    Change go-mode

--- .emacs.d/inits/33go-mode.el
+++ .emacs.d/inits/33go-mode.el
@@ -1,16 +1,17 @@
 ;; go-mode
-(require 'go-projectile)
-(require 'go-autocomplete)
+(eval-after-load "go-mode"
+  '(progn
+     (require 'go-projectile)
+     (require 'go-autocomplete)
 
-(add-hook 'go-mode-hook
-          '(lambda()
-	     (setq gofmt-command "goimports")
-	     (add-hook 'before-save-hook 'gofmt-before-save)
-             (setq c-basic-offset 4)
-             (setq indent-tabs-mode t)
-             (local-set-key (kbd "M-.") 'godef-jump)
-	     (go-eldoc-setup)))
+     (add-hook 'go-mode-hook
+	       '(lambda()
+		  (setq gofmt-command "goimports")
+		  (add-hook 'before-save-hook 'gofmt-before-save)
+		  (setq c-basic-offset 4)
+		  (setq indent-tabs-mode t)
+		  (local-set-key (kbd "M-.") 'godef-jump)
+		  (go-eldoc-setup)))
 
-(autoload 'helm-go-package "helm-go-package")
-(eval-after-load 'go-mode
-  '(substitute-key-definition 'go-import-add 'helm-go-package go-mode-map))
+     (substitute-key-definition 'go-import-add 'helm-go-package go-mode-map)
+     (autoload 'helm-go-package "helm-go-package")))
