565f470f65d10b63ca44dbdc5913e53ea10f8923
Masashi Miyaura <masasam@users.noreply.github.com>
2017-11-29 16:17:06 +0900

    Fix to with-eval-after-load

--- .emacs.d/inits/33go-mode.el
+++ .emacs.d/inits/33go-mode.el
@@ -1,17 +1,16 @@
 ;; go-mode
-(eval-after-load "go-mode"
-  '(progn
-     (require 'go-projectile)
-     (require 'go-autocomplete)
+(with-eval-after-load 'go-mode
+  (require 'go-projectile)
+  (require 'go-autocomplete)
 
-     (add-hook 'go-mode-hook
-	       '(lambda()
-		  (setq gofmt-command "goimports")
-		  (add-hook 'before-save-hook 'gofmt-before-save)
-		  (setq c-basic-offset 4)
-		  (setq indent-tabs-mode t)
-		  (local-set-key (kbd "M-.") 'godef-jump)
-		  (go-eldoc-setup)))
+  (add-hook 'go-mode-hook
+	    '(lambda()
+	       (setq gofmt-command "goimports")
+	       (add-hook 'before-save-hook 'gofmt-before-save)
+	       (setq c-basic-offset 4)
+	       (setq indent-tabs-mode t)
+	       (local-set-key (kbd "M-.") 'godef-jump)
+	       (go-eldoc-setup)))
 
-     (substitute-key-definition 'go-import-add 'helm-go-package go-mode-map)
-     (autoload 'helm-go-package "helm-go-package")))
+  (substitute-key-definition 'go-import-add 'helm-go-package go-mode-map)
+  (autoload 'helm-go-package "helm-go-package"))
