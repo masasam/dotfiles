5b2cfda3c48705dc266a97fa27ad6fcc64fecbba
Masashi Miyaura <masasam@users.noreply.github.com>
2017-12-03 19:09:40 +0900

    Reversed show-trailing-whitespace

--- .emacs.d/inits/71white-space.el
+++ .emacs.d/inits/71white-space.el
@@ -1,13 +1,14 @@
 ;; Highlight the space at the end of the line
-(setq-default show-trailing-whitespace t)
+(setq-default show-trailing-whitespace nil)
 
 (defun my/disable-trailing-mode ()
   "Disable show tail whitespace."
-  (setq show-trailing-whitespace nil))
+  (setq show-trailing-whitespace t))
 
-(add-hook 'term-mode-hook 'my/disable-trailing-mode)
-(add-hook 'eshell-mode-hook 'my/disable-trailing-mode)
-(add-hook 'markdown-mode-hook 'my/disable-trailing-mode)
+(add-hook 'emacs-lisp-mode-hook 'my/disable-trailing-mode)
+(add-hook 'python-mode-hook 'my/disable-trailing-mode)
+(add-hook 'c-mode-hook 'my/disable-trailing-mode)
+(add-hook 'shell-mode-hook 'my/disable-trailing-mode)
 
 
 ;; Remove contiguous line breaks at end of line + end of file
