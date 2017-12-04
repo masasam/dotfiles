20140a199837c25f79e7e6784cc958f53c7e33e2
Masashi Miyaura <masasam@users.noreply.github.com>
2017-12-04 15:37:42 +0900

    Changed helm-tramp and helm-ghq to run with counsel

---
+++ .emacs.d/inits/12tramp-ghq.el
@@ -0,0 +1,18 @@
+;; helm-tramp
+(require 'helm-tramp)
+(defun counsel-tramp ()
+  (interactive)
+  (counsel-find-file (ivy-read "Tramp:" (helm-tramp--candidates))))
+(defalias 'quit-tramp 'tramp-cleanup-all-buffers)
+(bind-key "C-c s" 'counsel-tramp)
+(eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
+(setq helm-tramp-docker-user '("admin" "user" "masa"))
+
+
+;; helm-ghq
+(bind-key "C-x l" 'counsel-ghq)
+(bind-key "C-x C-l" 'counsel-ghq)
+(require 'helm-ghq)
+(defun counsel-ghq ()
+  (interactive)
+  (counsel-find-file (ivy-read "ghq list:" (helm-ghq--list-candidates))))
