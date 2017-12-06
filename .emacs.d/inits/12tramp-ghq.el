78ae6cdda3a1c6c9c77386744a17329fc796bae6
Masashi Miyaura <masasam@users.noreply.github.com>
2017-12-06 22:01:34 +0900

    Implement counsel-ghq

--- .emacs.d/inits/12tramp-ghq.el
+++ .emacs.d/inits/12tramp-ghq.el
@@ -7,7 +7,21 @@
 ;; helm-ghq run with counsel
 (bind-key "C-x l" 'counsel-ghq)
 (bind-key "C-x C-l" 'counsel-ghq)
-(require 'helm-ghq)
+
+(defun counsel-ghq--list-candidates ()
+  (with-temp-buffer
+    (unless (zerop (apply #'call-process
+			  "ghq" nil t nil
+			  '("list" "--full-path")))
+      (error "Failed: Can't get ghq list candidates"))
+    (let ((paths))
+      (goto-char (point-min))
+      (while (not (eobp))
+	(push (buffer-substring-no-properties
+	       (line-beginning-position) (line-end-position)) paths)
+        (forward-line 1))
+      (reverse paths))))
+
 (defun counsel-ghq ()
   (interactive)
-  (counsel-find-file (ivy-read "ghq list: " (helm-ghq--list-candidates))))
+  (counsel-find-file (ivy-read "ghq list: " (counsel-ghq--list-candidates))))
