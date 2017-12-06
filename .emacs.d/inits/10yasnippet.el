501c000387d57aaf3972810484138347d4564c24
Masashi Miyaura <masasam@users.noreply.github.com>
2017-12-06 22:10:05 +0900

    Delete helm-c-yasnippet

--- .emacs.d/inits/10yasnippet.el
+++ .emacs.d/inits/10yasnippet.el
@@ -1,6 +1,3 @@
 (require 'yasnippet)
-(require 'helm-c-yasnippet)
-(setq helm-yas-space-match-any-greedy t)
-(bind-key "C-c y" 'helm-yas-complete)
 (push '("emacs.+/snippets/" . snippet-mode) auto-mode-alist)
 (yas-global-mode 1)
