4ed7b110f725d20d6a28bc52059b7506a0d30cdd
Masashi Miyaura <masasam@users.noreply.github.com>
2017-12-07 19:29:48 +0900

    Fix color

--- .emacs.d/inits/99_custom.el
+++ .emacs.d/inits/99_custom.el
@@ -4,7 +4,7 @@
 (load-theme 'material t)
 ;; Change part of theme to your liking.
 ;; Investigate by changing 'M-x list-faces-display'
-(setq cursor ((t (:background "#82c600")))
-      elscreen-tab-background-face ((t (:background "#37474f")))
-      elscreen-tab-current-screen-face ((t (:background "LightSkyBlue4" :foreground "white")))
-      elscreen-tab-other-screen-face ((t (:background "#37474f" :foreground "Gray50"))))
+(custom-set-faces '(cursor ((t (:background "#82c600"))))
+		  '(elscreen-tab-background-face ((t (:background "#37474f"))))
+		  '(elscreen-tab-current-screen-face ((t (:background "LightSkyBlue4" :foreground "white"))))
+		  '(elscreen-tab-other-screen-face ((t (:background "#37474f" :foreground "Gray50")))))
