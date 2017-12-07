779fbb84ac1fecb2660deaaf85645be4b716bc3a
Masashi Miyaura <masasam@users.noreply.github.com>
2017-12-07 20:43:12 +0900

    Update rust-mode

--- .emacs.d/inits/45rust.el
+++ .emacs.d/inits/45rust.el
@@ -1,6 +1,3 @@
-(setq racer-rust-src-path "~/Dropbox/emacs/rustc-1.14.0/src/")
-(add-to-list 'exec-path (expand-file-name "~/.cargo/bin/"))
-;; When rust-format-on-save is set to t in rust-mode, rustfmt runs automatically
 (with-eval-after-load 'rust-mode
   (setq-default rust-format-on-save t))
 ;; Launch racer and flycheck when editing rust files
