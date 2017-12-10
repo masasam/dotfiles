824edfcb213d5f8bd42c6e07603160a5e79e15bf
Masashi Miyaura <masasam@users.noreply.github.com>
2017-12-10 18:57:45 +0900

    Add company-statistics

--- .emacs.d/inits/06company.el
+++ .emacs.d/inits/06company.el
@@ -1,10 +1,8 @@
 (require 'company)
 
 ;; general
-;;(setq comany-idle-delay 0.1)
 (setq comany-minimum-prefix-length 1)
 (setq comany-selection-wrap-around t)
-;; (setq company-idle-delay nil) ; Do not autocomplete
 (bind-key "C-M-i" 'company-complete)
 (bind-key "C-h" nil company-active-map)
 (bind-key "C-n" 'company-select-next company-active-map)
@@ -14,3 +12,4 @@
 (bind-key "<tab>" 'company-complete-common-or-cycle company-active-map)
 (bind-key "M-d" 'company-show-doc-buffer company-active-map)
 (add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'after-init-hook 'company-statistics-mode)
