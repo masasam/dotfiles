f5200763f5ccc4759c9ca4585a2adbbe0eb1520f
Masashi Miyaura <masasam@users.noreply.github.com>
2017-12-13 15:05:42 +0900

    Delete sql-indent

--- .emacs.d/inits/30sql.el
+++
@@ -1,8 +0,0 @@
-(require 'sql-indent)
-
-(setq auto-mode-alist
-      (cons '("\\.sql$" . sql-mode) auto-mode-alist))
-
-(add-hook 'sql-interactive-mode-hook
-          (lambda ()
-            (toggle-truncate-lines t)))
