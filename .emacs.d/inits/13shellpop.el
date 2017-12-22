d4dbf393b608e207d161838bf9d1830949e5f8e0
Masashi Miyaura <masasam@users.noreply.github.com>
2017-12-23 04:58:52 +0900

    Delete shell-pop

--- .emacs.d/inits/13shellpop.el
+++
@@ -1,8 +0,0 @@
-(setq
- shell-pop-default-directory "~"
- shell-pop-shell-type (quote ("ansi-term" "*ansi-term*" (lambda nil (ansi-term shell-pop-term-shell))))
- shell-pop-term-shell "/bin/zsh"
- shell-pop-universal-key "C-t"
- shell-pop-window-size 30
- shell-pop-full-span t
- shell-pop-window-position "bottom")
