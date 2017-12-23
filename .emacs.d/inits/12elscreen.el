c39c27f52db5bed014b141d948eb08a90ccc03a8
Masashi Miyaura <masasam@users.noreply.github.com>
2017-12-23 09:37:37 +0900

    Delete elscreen

--- .emacs.d/inits/12elscreen.el
+++
@@ -1,8 +0,0 @@
-;; prefix key
-(setq elscreen-prefix-key (kbd "C-T"))
-;; Do not display [X] at the beginning of the tab
-(setq elscreen-tab-display-kill-screen nil)
-;; Do not display [<->] at the beginning of header-line
-(setq elscreen-tab-display-control nil)
-;; Don't show tab number in mode-line
-(setq elscreen-display-screen-number nil)
