53a5268914c1f3e2c019255f6449028b7b0a6798
Masashi Miyaura <masasam@users.noreply.github.com>
2017-12-23 11:41:59 +0900

    Fix company-mode

--- .emacs.d/inits/05company.el
+++ .emacs.d/inits/05company.el
@@ -1,7 +1,7 @@
 (require 'company)
 
 ;; general
-(setq comany-minimum-prefix-length 1)
+(setq comany-minimum-prefix-length 3)
 (setq comany-selection-wrap-around t)
 (bind-key "C-M-i" 'company-complete)
 (bind-key "C-h" nil company-active-map)
@@ -10,6 +10,7 @@
 (bind-key "C-n" 'company-select-next company-search-map)
 (bind-key "C-p" 'company-select-previous company-search-map)
 (bind-key "<tab>" 'company-complete-common-or-cycle company-active-map)
+(bind-key "C-i" 'company-complete-selection company-active-map)
 (bind-key "M-d" 'company-show-doc-buffer company-active-map)
 (add-hook 'after-init-hook 'global-company-mode)
 (add-hook 'after-init-hook 'company-statistics-mode)
