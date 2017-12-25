16cef64a56d0b1ced34b542508770a59d536e186
Masashi Miyaura <masasam@users.noreply.github.com>
2017-12-25 19:31:57 +0900

    Delete unnecessary

--- .emacs.d/inits/99_custom.el
+++ .emacs.d/inits/99_custom.el
@@ -5,7 +5,4 @@
 ;; Change part of theme to your liking.
 ;; Investigate by changing 'M-x list-faces-display'
 (custom-set-faces '(cursor ((t (:background "#82c600"))))
-		  '(elscreen-tab-background-face ((t (:background "#37474f"))))
-		  '(elscreen-tab-current-screen-face ((t (:background "LightSkyBlue4" :foreground "white"))))
-		  '(elscreen-tab-other-screen-face ((t (:background "#37474f" :foreground "Gray50"))))
 		  '(helm-candidate-number ((t (:background "#1c1f26" :foreground "#ffffff")))))
