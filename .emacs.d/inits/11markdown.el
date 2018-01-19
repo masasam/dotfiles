bf4d5e63da806fdda6d9216c571644a6642aff55
Masashi Miyaura <masasam@users.noreply.github.com>
2018-01-19 22:38:35 +0900

    Comment out pangu-spacing

--- .emacs.d/inits/11markdown.el
+++ .emacs.d/inits/11markdown.el
@@ -2,18 +2,18 @@
           '(lambda () (outline-minor-mode t)))
 
 
-;; pangu-spacing
-;; Insert a space between full-width and half-width letters
-(setq pangu-spacing-chinese-before-english-regexp
-      (rx (group-n 1 (category japanese))
-	  (group-n 2 (in "a-zA-Z0-9"))))
-(setq pangu-spacing-chinese-after-english-regexp
-      (rx (group-n 1 (in "a-zA-Z0-9"))
-	  (group-n 2 (category japanese))))
-;; Place the actual space rather than the appearance
-(setq pangu-spacing-real-insert-separtor t)
-;; use markdown-mode
-(add-hook 'markdown-mode-hook 'pangu-spacing-mode)
+;; ;; pangu-spacing
+;; ;; Insert a space between full-width and half-width letters
+;; (setq pangu-spacing-chinese-before-english-regexp
+;;       (rx (group-n 1 (category japanese))
+;; 	  (group-n 2 (in "a-zA-Z0-9"))))
+;; (setq pangu-spacing-chinese-after-english-regexp
+;;       (rx (group-n 1 (in "a-zA-Z0-9"))
+;; 	  (group-n 2 (category japanese))))
+;; ;; Place the actual space rather than the appearance
+;; (setq pangu-spacing-real-insert-separtor t)
+;; ;; use markdown-mode
+;; (add-hook 'markdown-mode-hook 'pangu-spacing-mode)
 
 
 ;; markdown-preview like github
