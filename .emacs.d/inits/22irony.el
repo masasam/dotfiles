281e3c96115a34150cec9b69d3fcaaae1c5d1635
Masashi Miyaura <masasam@users.noreply.github.com>
2018-02-22 18:16:56 +0900

    Fix irony-mode

--- .emacs.d/inits/22irony.el
+++ .emacs.d/inits/22irony.el
@@ -1,6 +1,11 @@
-(add-hook 'c-mode-hook 'irony-mode)
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'objc-mode-hook 'irony-mode)
+(defun my-irony-mode-on ()
+  ;; avoid enabling irony-mode in modes that inherits c-mode, e.g: php-mode
+  (when (member major-mode irony-supported-major-modes)
+    (irony-mode 1)))
+
+(add-hook 'c-mode-hook 'my-irony-mode-on)
+(add-hook 'c++-mode-hook 'my-irony-mode-on)
+(add-hook 'objc-mode-hook 'my-irony-mode-on)
 (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 (add-to-list 'company-backends 'company-irony)
 (add-hook 'irony-mode-hook #'irony-eldoc)
