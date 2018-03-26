066263a50461858a263bceefa339d7812e3d95a4
Masashi Miyaura <masasam@users.noreply.github.com>
2018-03-26 15:56:24 +0900

    Add lispy-mode for elisp

--- .emacs.d/inits/20elisp.el
+++ .emacs.d/inits/20elisp.el
@@ -5,14 +5,6 @@
 (bind-key "C-c e" 'macrostep-expand emacs-lisp-mode-map)
 (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
 
-;; paredit
-;; (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-;; (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
-;; (add-hook 'lisp-mode-hook 'enable-paredit-mode)
-;; (add-hook 'ielm-mode-hook 'enable-paredit-mode)
-;; (add-hook 'scheme-mode-hook 'enable-paredit-mode)
-;; (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
-;; (defun override-slime-repl-bindings-with-paredit ()
-;;   (define-key slime-repl-mode-map
-;;     (read-kbd-macro paredit-backward-delete-key) nil))
-;; (add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
+(add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
+(add-hook 'lisp-interaction-mode-hook (lambda () (lispy-mode 1)))
+(add-hook 'lisp-mode-hook (lambda () (lispy-mode 1)))
