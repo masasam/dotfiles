b341b835de5f4411bcd2da64251aee8cc18f8473
Masashi Miyaura <masasam@users.noreply.github.com>
2018-05-08 18:03:55 +0900

    Change preview time

--- .emacs.d/inits/62easy-hugo.el
+++ .emacs.d/inits/62easy-hugo.el
@@ -1,6 +1,6 @@
 (setq easy-hugo-basedir "~/src/github.com/masasam/blog/")
 (setq easy-hugo-sshdomain "blogdomain")
 (setq easy-hugo-root "/home/blog/")
-(setq easy-hugo-previewtime "300")
+(setq easy-hugo-previewtime "50")
 (setq easy-hugo-default-picture-directory "~/Pictures")
 (define-key global-map (kbd "C-c C-e") 'easy-hugo)
