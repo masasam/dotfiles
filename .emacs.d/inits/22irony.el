aebb326c68651715e2b4e03118e43169e73611f4
Masashi Miyaura <masasam@users.noreply.github.com>
2018-09-15 14:37:05 +0900

    Add rtags

--- .emacs.d/inits/22irony.el
+++ .emacs.d/inits/22irony.el
@@ -9,3 +9,14 @@
 (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 (add-to-list 'company-backends 'company-irony)
 (add-hook 'irony-mode-hook #'irony-eldoc)
+
+
+;; rtags
+(when (require 'rtags nil 'noerror)
+  (add-hook 'c-mode-common-hook
+            (lambda ()
+              (when (rtags-is-indexed)
+                (local-set-key (kbd "M-.") 'rtags-find-symbol-at-point)
+                (local-set-key (kbd "M-;") 'rtags-find-symbol)
+                (local-set-key (kbd "M-@") 'rtags-find-references)
+                (local-set-key (kbd "M-,") 'rtags-location-stack-back)))))
