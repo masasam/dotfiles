abc30020b296f1ac0585047054e83c97cfaf58f1
Masashi Miyaura <masasam@users.noreply.github.com>
2018-09-26 17:38:37 +0900

    Add hydra-view

--- .emacs.d/inits/12hydra.el
+++ .emacs.d/inits/12hydra.el
@@ -29,10 +29,11 @@
 _j_: next-line   _k_: previous-line    _h_: one line    _l_: forward-char  _i_: view-mode
 _g_: beginning-of-buffer   _G_: end-of-buffer  _a_: beginning-of-line   _e_: end-of-line
 _b_: delete-window   _x_: delite-window   _0_: delete-window   _1_: delete-other-windows
-_n_: scroll-up-command   _p_: scroll-down-command
-_2_: other-window-or-split
-_/_: swiper-for-region-or-swiper  _f_: View-scroll-line-forward
-_3_: other-window-or-split-horizon  _o_:  other-window-or-split
+_n_: scroll-up-command   _p_: scroll-down-command   _u_: View-scroll-half-page-backward
+_2_: other-window-or-split   _d_: View-scroll-half-page-forward   _._: set-mark-command
+_/_: swiper-for-region-or-swiper  _f_: View-scroll-line-forward   _@_: View-back-to-mark
+_3_: other-window-or-split-horizon  _o_:  other-window-or-split   _%_: View-goto-percent
+_d_: View-scroll-half-page-forward  _c_: View-kill-and-leave   _=_: what-line
 "
   ("j" 'next-line)
   ("k" 'previous-line)
@@ -49,6 +50,13 @@ _3_: other-window-or-split-horizon  _o_:  other-window-or-split
   ("f" 'View-scroll-line-forward)
   ("b" 'View-scroll-line-backward)
   ("x" 'delete-window)
+  ("u" 'View-scroll-half-page-backward)
+  ("d" 'View-scroll-half-page-forward)
+  ("c" 'View-kill-and-leave)
+  ("@" 'View-back-to-mark)
+  ("." 'set-mark-command)
+  ("%" 'View-goto-percent)
+  ("=" 'what-line)
   ("0" 'delete-window)
   ("1" 'delete-other-windows)
   ("2" 'other-window-or-split)
