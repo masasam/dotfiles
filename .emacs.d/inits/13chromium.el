6927835aae07dd8b51cf74bad8686d586ad36ab5
Masashi Miyaura <masasam@users.noreply.github.com>
2018-10-15 12:14:32 +0900

    Add chromium command

---
+++ .emacs.d/inits/13chromium.el
@@ -0,0 +1,52 @@
+(defun chromium-calendar ()
+  "Open google-calendar with chromium."
+  (interactive)
+  (browse-url "https://calendar.google.com/calendar/r"))
+
+
+(defun chromium-gmail ()
+  "Open gmail with chromium."
+  (interactive)
+  (browse-url "https://mail.google.com/mail/u/0/#inbox"))
+
+
+(defun chromium-drive ()
+  "Open google-drive with chromium."
+  (interactive)
+  (browse-url "https://drive.google.com/drive/u/0/my-drive"))
+
+
+(defun chromium-github ()
+  "Open github with chromium."
+  (interactive)
+  (browse-url "https://github.com/masasam"))
+
+
+(defun chromium-reddit ()
+  "Open reddit with chromium."
+  (interactive)
+  (browse-url "https://www.reddit.com"))
+
+
+(defun chromium-hackernews ()
+  "Open hackernews with chromium."
+  (interactive)
+  (browse-url "https://news.ycombinator.com"))
+
+
+(defun chromium-maps ()
+  "Open google-maps with chromium."
+  (interactive)
+  (browse-url "https://www.google.co.jp/maps"))
+
+
+(defun chromium-pocket ()
+  "Open pocket with chromium."
+  (interactive)
+  (browse-url "https://getpocket.com/a/queue/"))
+
+
+(defun chromium-twitter ()
+  "Open twitter with chromium."
+  (interactive)
+  (browse-url "https://twitter.com"))
