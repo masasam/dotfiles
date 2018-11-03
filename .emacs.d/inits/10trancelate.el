a1bfbe409f080157b651fdd0a543ec924143ab97
Masashi Miyaura <masasam@users.noreply.github.com>
2018-11-04 01:44:57 +0900

    Fix google translate

--- .emacs.d/inits/10trancelate.el
+++ .emacs.d/inits/10trancelate.el
@@ -1,7 +1,7 @@
 (require 'google-translate)
 (require 'google-translate-default-ui)
 
-(bind-key "C-c t" 'google-translate-enja-or-jaen)
+(bind-key "C-c t" 'google-translate-auto)
 
 (defvar toggle-translate-flg nil
   "Toggle flg.")
@@ -11,31 +11,38 @@
   (interactive)
   (if toggle-translate-flg
       (progn
-	(bind-key "C-c t" 'google-translate-enja-or-jaen)
+	(bind-key "C-c t" 'google-translate-auto)
 	(setq toggle-translate-flg nil))
     (progn
       (bind-key "C-c t" 'chromium-translate)
       (setq toggle-translate-flg t))))
 
-(defun google-translate-enja-or-jaen (&optional string)
-  "Translate words in region or current position. Can also specify query with C-u"
+
+(defun google-translate-auto ()
+  "Automatically recognize and translate Japanese and English."
   (interactive)
-  (setq string
-        (cond ((stringp string) string)
-              (current-prefix-arg
-               (read-string "Google Translate: "))
-              ((use-region-p)
-               (buffer-substring (region-beginning) (region-end)))
-              (t
-               (thing-at-point 'word))))
-  (let* ((asciip (string-match
-                  (format "\\`[%s]+\\'" "[:ascii:]")
-                  string)))
-    (run-at-time 0.1 nil 'deactivate-mark)
-    (google-translate-translate
-     (if asciip "en" "ja")
-     (if asciip "ja" "en")
-     string)))
+  (if (region-active-p)
+      (progn (setq mark-active nil)
+	     (if (string-match (format "\\`[%s]+\\'" "[:ascii:]")
+			       (buffer-substring (region-beginning) (region-end)))
+		 (google-translate-translate
+		  "en" "ja"
+		  (buffer-substring
+		   (region-beginning) (region-end)))
+	       (google-translate-translate
+		"ja" "en"
+		(buffer-substring
+		 (region-beginning) (region-end)))))
+    (let ((string (read-string "Google Translate: ")))
+      (if (string-match
+	   (format "\\`[%s]+\\'" "[:ascii:]")
+	   string)
+	  (google-translate-translate
+	   "en" "ja"
+	   string)
+	(google-translate-translate
+	 "ja" "en"
+	 string)))))
+
 
 (push "*Google Translate*" popwin:special-display-config)
-
