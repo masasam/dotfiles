a140afadf01dc832eb10ef9f719a45781f4d3a89
Masashi Miyaura <masasam@users.noreply.github.com>
2018-11-19 11:52:36 +0900

    Add diminish setting

---
+++ .emacs.d/inits/14diminish.el
@@ -0,0 +1,21 @@
+;;; 14diminish.el --- 14diminish.el
+;;; Commentary:
+;;; Code:
+;;(setq debug-on-error t)
+
+(diminish 'abbrev-mode)
+(diminish 'back-button-mode)
+(diminish 'beginend-global-mode)
+(diminish 'beginend-prog-mode)
+(diminish 'editorconfig-mode)
+(diminish 'elisp-slime-nav-mode)
+(diminish 'flycheck-mode)
+(diminish 'git-gutter-mode)
+(diminish 'ivy-mode)
+(diminish 'page-break-lines-mode)
+(diminish 'projectile-mode)
+(diminish 'projectile-rails-mode)
+(diminish 'smartparens-mode)
+(diminish 'volatile-highlights-mode)
+
+;;; 14diminish.el ends here
