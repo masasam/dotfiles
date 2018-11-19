f11f971bd976e7cb65cfd7f22e5900f46ab09b07
Masashi Miyaura <masasam@users.noreply.github.com>
2018-11-19 13:36:48 +0900

    Add web-mode setting

---
+++ .emacs.d/inits/30web.el
@@ -0,0 +1,34 @@
+;;; 30web.el --- 30web.el
+;;; Commentary:
+;;; Code:
+;;(setq debug-on-error t)
+
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-comment-style 2)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-style-padding 1)
+  (setq web-mode-script-padding 1)
+  (setq web-mode-block-padding 0)
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-css-colorization t)
+  (setq web-mode-enable-heredoc-fontification t)
+  (setq web-mode-enable-current-column-highlight t)
+  (setq web-mode-tag-auto-close-style 2))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
+;;; 30web.el ends here
