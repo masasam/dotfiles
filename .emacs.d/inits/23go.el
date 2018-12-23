29af8769825ed11089b8d590ca2b06761154885c
Masashi Miyaura <masasam@users.noreply.github.com>
2018-12-23 18:08:23 +0900

    Fix typo

--- .emacs.d/inits/23go.el
+++ .emacs.d/inits/23go.el
@@ -5,17 +5,11 @@
 
 ;; go-mode
 (with-eval-after-load 'go-mode
-  (require 'go-projectile)
-  (require 'company-go)
-
+  (add-hook 'go-mode-hook 'eglot-ensure)
+  (add-hook 'before-save-hook #'gofmt-before-save)
   (add-hook 'go-mode-hook
 	    '(lambda()
-	       (set (make-local-variable 'company-backends) '(company-go))
-	       (setq gofmt-command "goimports")
-	       (add-hook 'before-save-hook 'gofmt-before-save)
 	       (setq c-basic-offset 4)
-	       (setq indent-tabs-mode t)
-	       (local-set-key (kbd "M-.") 'godef-jump)
-	       (go-eldoc-setup))))
+	       (setq indent-tabs-mode t))))
 
 ;;; 23go.el ends here
