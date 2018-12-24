051d68970eae23e9b248dd67a020a0aa720d9f23
Masashi Miyaura <masasam@users.noreply.github.com>
2018-12-24 21:44:16 +0900

    Delete flycheck-rust etc

--- .emacs.d/inits/24rust.el
+++ .emacs.d/inits/24rust.el
@@ -3,18 +3,6 @@
 ;;; Code:
 ;;(setq debug-on-error t)
 
-(with-eval-after-load 'rust-mode
-  (setq-default rust-format-on-save t))
-;; Launch racer and flycheck when editing rust files
-(add-hook 'rust-mode-hook (lambda ()
-                            (racer-mode)
-                            (flycheck-rust-setup)))
-;; Use racer's eldoc support
-(add-hook 'racer-mode-hook #'eldoc-mode)
-;; Use racer's supplementary support
-(add-hook 'racer-mode-hook (lambda ()
-                             (company-mode-on)
-                             (set (make-variable-buffer-local 'company-idle-delay) 0.1)
-                             (set (make-variable-buffer-local 'company-minimum-prefix-length) 1)))
+(add-hook 'rustic-mode 'eglot-ensure)
 
 ;;; 24rust.el ends here
