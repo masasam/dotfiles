f5b967c52cf40cd00b86b349dd099ac06dfa003f
Masashi Miyaura <masasam@users.noreply.github.com>
2018-12-24 11:39:33 +0900

    Change irony to eglot

--- .emacs.d/inits/22cmode.el
+++ .emacs.d/inits/22cmode.el
@@ -1,2 +1,12 @@
+;;; 22cmode.el --- 22cmode.el
+;;; Commentary:
+;;; Code:
+;;(setq debug-on-error t)
+
 (add-hook 'c-mode-hook 'eglot-ensure)
-(add-hook 'c++mode-hook 'eglot-ensure)
+(add-hook 'c++-mode-hook 'eglot-ensure)
+
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
+;;; 22cmode.el ends here
