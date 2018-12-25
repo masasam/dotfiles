9a4cd7b496ba116a41cc0e27e3d70b0d39a31dbd
Masashi Miyaura <masasam@users.noreply.github.com>
2018-12-25 09:49:30 +0900

    Chenge robe to eglot

--- .emacs.d/inits/28ruby.el
+++ .emacs.d/inits/28ruby.el
@@ -7,26 +7,14 @@
 (setq rbenv-installation-dir "/home/masa/.rbenv")
 (global-rbenv-mode)
 
-(add-to-list 'auto-mode-alist
-             '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
-
-(add-hook 'enh-ruby-mode-hook 'robe-mode)
-(add-hook 'enh-ruby-mode-hook 'inf-ruby-minor-mode)
-
-;; robe
-;;(autoload 'robe-mode "robe" "Code navigation, documentation lookup and completion for Ruby" t nil)
-
-(eval-after-load 'company
-  '(push 'company-robe company-backends))
-
-(setq enh-ruby-add-encoding-comment-on-save nil)
-
-;; rspec-mode
-(eval-after-load 'rspec-mode
-  '(rspec-install-snippets))
+(add-hook 'ruby-mode-hook 'eglot-ensure)
 
 ;; projectile rails
 (projectile-rails-global-mode)
+
+;; (add-to-list 'auto-mode-alist
+;;              '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
+;; (setq enh-ruby-add-encoding-comment-on-save nil)
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
