bac0bda6c978a59947cad08fcefa98cb5a970504
Masashi Miyaura <masasam@users.noreply.github.com>
2019-01-10 01:34:24 +0900

    Add go-langserver setting

--- .emacs.d/inits/23go.el
+++ .emacs.d/inits/23go.el
@@ -4,12 +4,13 @@
 ;;(setq debug-on-error t)
 
 ;; go-mode
-(with-eval-after-load 'go-mode
-  (add-hook 'go-mode-hook 'eglot-ensure)
-  (add-hook 'before-save-hook #'gofmt-before-save)
-  (add-hook 'go-mode-hook
-	    '(lambda()
-	       (setq c-basic-offset 4)
-	       (setq indent-tabs-mode t))))
+(add-hook 'go-mode-hook 'eglot-ensure)
+(add-hook 'before-save-hook #'gofmt-before-save)
+(add-hook 'go-mode-hook
+	  '(lambda()
+	     (setq c-basic-offset 4)
+	     (setq indent-tabs-mode t)))
+
+(add-to-list 'eglot-server-programs '(go-mode . ("go-langserver" "-gocodecompletion")))
 
 ;;; 23go.el ends here
