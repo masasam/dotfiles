a94f69a4863053c097fcb5671366e47d3b2f7021
Masashi Miyaura <masasam@users.noreply.github.com>
2019-03-13 17:48:36 +0900

    Change eldoc-idle-delay

--- .emacs.d/inits/08flycheck.el
+++ .emacs.d/inits/08flycheck.el
@@ -32,7 +32,7 @@
 
 
 ;; eldoc
-(setq eldoc-idle-delay 0)
+(setq eldoc-idle-delay 0.50)
 (setq eldoc-echo-area-use-multiline-p t)
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
