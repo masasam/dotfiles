090056866bc0c1145c4483005e3363e002244b96
Masashi Miyaura <masasam@users.noreply.github.com>
2019-03-15 16:39:08 +0900

    Change to refer editorconfig

--- .emacs.d/inits/30web.el
+++ .emacs.d/inits/30web.el
@@ -15,7 +15,7 @@
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
   (setq web-mode-comment-style 2)
-  (setq web-mode-markup-indent-offset 2)
+  ;; (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
   (setq web-mode-style-padding 1)
