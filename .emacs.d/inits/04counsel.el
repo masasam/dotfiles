a86703e12b2ff4483aeaa2d4b9fce0677ae46252
Masashi Miyaura <masasam@users.noreply.github.com>
2019-04-18 18:56:44 +0900

    Add contribute setting

--- .emacs.d/inits/04counsel.el
+++ .emacs.d/inits/04counsel.el
@@ -8,7 +8,7 @@
 (setq ivy-use-selectable-prompt t)
 (setq enable-recursive-minibuffers t)
 (setq company-selection-wrap-around t)
-(bind-key "C-s" 'swiper-for-region-or-swiper)
+(bind-key "C-s" 'swiper-isearch-region-or-not)
 (bind-key "C-;" 'counsel-switch-buffer)
 (bind-key "C-c C-r" 'ivy-resume)
 (bind-key "C-x C-c" 'counsel-M-x)
@@ -34,16 +34,37 @@
 
 (setq swiper-include-line-number-in-search t)
 
-(defun swiper-for-region-or-swiper ()
-  "If the region is active, swiper-isearch for region.
-If the region is inactive, swiper-isearch."
+(defun swiper-isearch-region-or-not ()
+  "If region is selected, `swiper-isearch' with the keyword selected in region.
+If the region isn't selected, `swiper-isearch'."
   (interactive)
-  (if (region-active-p)
-      (progn (setq mark-active nil)
-	     (swiper-isearch (buffer-substring
-			      (region-beginning) (region-end))))
-    (swiper-isearch)))
+  (if (not (use-region-p))
+      (swiper-isearch)
+    (deactivate-mark)
+    (swiper-isearch (buffer-substring-no-properties
+                     (region-beginning) (region-end)))))
 
+
+(defun swiper-region ()
+  "If region is selected, `swiper' with the keyword selected in region.
+If the region isn't selected, `swiper'."
+  (interactive)
+  (if (not (use-region-p))
+      (swiper)
+    (deactivate-mark)
+    (swiper (buffer-substring-no-properties
+             (region-beginning) (region-end)))))
+
+
+(defun swiper-isearch-region ()
+  "If region is selected, `swiper-isearch' with the keyword selected in region.
+If the region isn't selected, `swiper-isearch'."
+  (interactive)
+  (if (not (use-region-p))
+      (swiper-isearch)
+    (deactivate-mark)
+    (swiper-isearch (buffer-substring-no-properties
+                     (region-beginning) (region-end)))))
 
 ;; counsel-tramp
 (setq tramp-default-method "ssh")
