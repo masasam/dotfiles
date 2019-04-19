8b7f4395cc06f3313e3cc11006a044f3ecac861f
Masashi Miyaura <masasam@users.noreply.github.com>
2019-04-19 13:02:46 +0900

    Add setting for deadgrep

--- .emacs.d/inits/19wgrep.el
+++ .emacs.d/inits/19wgrep.el
@@ -7,4 +7,9 @@
 (add-hook 'ag-mode-hook 'wgrep-ag-setup)
 (setq wgrep-enable-key "e")
 
+
+;; for deadgrep-mode
+(bind-key "n" 'next-line deadgrep-mode-map)
+(bind-key "p" 'previous-line deadgrep-mode-map)
+
 ;;; 19wgrep ends here
