113c66c242d89b17845d41b992cc31d75edf640d
Masashi Miyaura <masasam@users.noreply.github.com>
2019-06-15 12:16:14 +0900

    Fix typo

--- .emacs.d/inits/00base.el
+++ .emacs.d/inits/00base.el
@@ -185,7 +185,7 @@ If the region is inactive, `backward-kill-word'."
 (customize-set-variable
  'tramp-ssh-controlmaster-options
  (concat
-  "-o ControlPath=/tmp/.ssh/ssh-ControlPath-%%r@%%h:%%p "
+  "-o ControlPath=/tmp/ssh-ControlPath-%%r@%%h:%%p "
   "-o ControlMaster=auto -o ControlPersist=yes"))
 
 (customize-set-variable 'tramp-use-ssh-controlmaster-options nil)
