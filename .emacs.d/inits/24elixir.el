5eda76b43f0c6207c02d106438453c82ae5219a3
Masashi Miyaura <masasam@users.noreply.github.com>
2019-06-19 01:39:51 +0900

    Fix typo

--- .emacs.d/inits/24elixir.el
+++ .emacs.d/inits/24elixir.el
@@ -4,7 +4,5 @@
 ;;(setq debug-on-error t)
 
 (add-hook 'elixir-mode-hook 'eglot-ensure)
-(add-to-list 'eglot-server-programs '(elixir-mode . ("sh" "/home/masa/src/github.com/JakeBecker/elixir-ls/rel/language_server.sh")))
-
 
 ;;; 24elixir.el ends here
