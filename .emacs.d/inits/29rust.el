043c07efe854253ed803e0dfc3602f583083d58b
Masashi Miyaura <masasam@users.noreply.github.com>
2019-06-23 15:26:41 +0900

    Fix typo

---
+++ .emacs.d/inits/29rust.el
@@ -0,0 +1,11 @@
+;;; 29rust.el --- 29rust.el
+;;; Commentary:
+;;; Code:
+;;(setq debug-on-error t)
+
+(add-hook 'rust-mode-hook 'eglot-ensure)
+
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
+;;; 29rust.el ends here
