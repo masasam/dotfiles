51df788ed1a1420a71f200f77844f1ecbace405d
Masashi Miyaura <masasam@users.noreply.github.com>
2019-08-18 08:00:26 +0900

    Fix from GTK+ to GTK

--- .emacs.d/inits/01dashboard.el
+++ .emacs.d/inits/01dashboard.el
@@ -9,7 +9,7 @@
   ;; Change start screen with dashboard
   (require 'dashboard)
   ;; Set the title
-  (setq dashboard-banner-logo-title (concat "GNU Emacs " emacs-version " kernel " (car (split-string (shell-command-to-string "uname -r") "-"))  " x86_64 ArchLinux GTK+ " gtk-version-string))
+  (setq dashboard-banner-logo-title (concat "GNU Emacs " emacs-version " kernel " (car (split-string (shell-command-to-string "uname -r") "-"))  " x86_64 ArchLinux GTK " gtk-version-string))
   ;; Set the banner
   (setq dashboard-startup-banner "~/src/github.com/masasam/dotfiles/.emacs.d/emacs.png")
   (dashboard-setup-startup-hook)
