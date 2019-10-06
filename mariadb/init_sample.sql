07ecfb450baac35f9d09b384a5848ad610ad26fe
Masashi Miyaura <masasam@users.noreply.github.com>
2019-10-06 18:29:02 +0900

    Add sample file

---
+++ mariadb/init_sample.sql
@@ -0,0 +1,4 @@
+set password for root@localhost=password("your_password");
+CREATE DATABASE database_name;
+USE database_name;
+SOURCE /home/masa/src/github.com/masasam/dotfiles/mariadb/initdump.sql;
