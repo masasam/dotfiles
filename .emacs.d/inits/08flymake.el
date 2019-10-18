356a0cfd12e281df1cd2950b8d8da1a616f7b8ba
Masashi Miyaura <masasam@users.noreply.github.com>
2019-10-18 12:30:06 +0900

    Implement flymake-posframe

--- .emacs.d/inits/08flymake.el
+++ .emacs.d/inits/08flymake.el
@@ -12,6 +12,31 @@
 		      :background "dark slate gray" :foreground "white" :underline nil))
 (remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake)
 
+;; flymake-posframe
+(defvar flymake-posframe-hide-posframe-hooks
+  '(pre-command-hook post-command-hook focus-out-hook)
+  "The hooks which should trigger automatic removal of the posframe.")
+
+(defun flymake-posframe-hide-posframe ()
+  "Hide messages currently being shown if any."
+  (posframe-hide " *flymake-posframe-buffer*")
+  (dolist (hook flymake-posframe-hide-posframe-hooks)
+    (remove-hook hook #'flymake-posframe-hide-posframe t)))
+
+(defun my/flymake-diagnostic-at-point-display-popup (text)
+  "Display the flymake diagnostic TEXT inside a posframe."
+  (posframe-show " *flymake-posframe-buffer*"
+		 :string (concat flymake-diagnostic-at-point-error-prefix
+				 (flymake--diag-text
+				  (get-char-property (point) 'flymake-diagnostic)))
+		 :position (point)
+		 :background-color "dark slate gray"
+		 :poshandler 'posframe-poshandler-window-bottom-left-corner)
+  (dolist (hook flymake-posframe-hide-posframe-hooks)
+    (add-hook hook #'flymake-posframe-hide-posframe nil t)))
+
+(advice-add 'flymake-diagnostic-at-point-display-popup :override 'my/flymake-diagnostic-at-point-display-popup)
+
 
 ;; flyspell-correct
 (require 'flyspell-correct-ivy)
