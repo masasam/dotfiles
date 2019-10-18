e2451756e5e4c73b620494edd601b9faf001845c
Masashi Miyaura <masasam@users.noreply.github.com>
2019-10-18 16:58:42 +0900

    Add eglot setting for sh-mode

---
+++ .emacs.d/inits/29sh.el
@@ -0,0 +1,11 @@
+;;; 29sh.el --- 29sh.el
+;;; Commentary:
+;;; Code:
+;;(setq debug-on-error t)
+
+(add-hook 'sh-mode-hook 'eglot-ensure)
+
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
+;;; 29sh.el ends here
