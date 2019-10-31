cb799a19f32b80969d32264fef3f5955c7070efd
Masashi Miyaura <masasam@users.noreply.github.com>
2019-10-31 11:35:22 +0900

    Fix web-mode

--- .emacs.d/inits/32web.el
+++ .emacs.d/inits/32web.el
@@ -3,6 +3,7 @@
 ;;; Code:
 ;;(setq debug-on-error t)
 
+(add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
@@ -15,11 +16,11 @@
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
   (setq web-mode-comment-style 2)
-  ;; (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
-  (setq web-mode-style-padding 1)
-  (setq web-mode-script-padding 1)
+  (setq web-mode-style-padding 0)
+  (setq web-mode-script-padding 0)
   (setq web-mode-block-padding 0)
   (setq web-mode-enable-auto-pairing t)
   (setq web-mode-enable-css-colorization t)
