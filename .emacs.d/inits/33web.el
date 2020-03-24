8ec6112326b385080acca6d2135ea1e0ca35c2fe
Masashi Miyaura <masasam@users.noreply.github.com>
2020-03-24 12:53:00 +0900

    Add tpl

--- .emacs.d/inits/33web.el
+++ .emacs.d/inits/33web.el
@@ -12,6 +12,7 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl?\\'" . web-mode))
 
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
