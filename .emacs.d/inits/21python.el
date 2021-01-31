d2eb971dcbe50c9be9d18a99ed0fd6d457c2e3f2
Masashi Miyaura <masasam@users.noreply.github.com>
2021-02-01 08:18:18 +0900

    Update package

--- .emacs.d/inits/21python.el
+++ .emacs.d/inits/21python.el
@@ -4,6 +4,9 @@
 ;;(setq debug-on-error t)
 
 (add-hook 'python-mode-hook 'eglot-ensure)
+((python-mode
+  . ((eglot-workspace-configuration
+      . ((:pyls . (:plugins (:jedi_completion (:include_params t)))))))))
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
