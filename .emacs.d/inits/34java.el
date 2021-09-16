aff25e2fce36737a609b6a1a6e87df5ec6aae608
Masashi Miyaura <masasam@users.noreply.github.com>
2021-09-16 20:32:06 +0900

    Add lsp-java

---
+++ .emacs.d/inits/34java.el
@@ -0,0 +1,12 @@
+;;; 34java.el --- 34java
+;;; Commentary:
+;;; Code:
+;;(setq debug-on-error t)
+
+(require 'lsp-java)
+(add-hook 'java-mode-hook #'lsp)
+
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
+;;; 34java ends here
