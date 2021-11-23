c5a9d2d8e62dab0fd6eeb64d42a4365b8cf07539
Masashi Miyaura <masasam@users.noreply.github.com>
2021-11-24 08:52:39 +0900

    Fix typo

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,8 +19,6 @@
        (proto (if no-ssl "http" "https")))
   ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
     (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
@@ -150,7 +148,7 @@
   (package-install 'nginx-mode)
   (package-install 'nodejs-repl)
   (package-install 'openwith)
-  (package-install 'org-plus-contrib)
+  (package-install 'org)
   (package-install 'package-lint)
   (package-install 'package-lint-flymake)
   (package-install 'packed)
