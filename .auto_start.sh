1071a4caf406d609d407a46d4a3523e0e61279c9
Masashi Miyaura <masasam@users.noreply.github.com>
2022-01-01 07:00:49 +0900

    Update

--- .auto_start.sh
+++ .auto_start.sh
@@ -1,7 +1,8 @@
+
 #!/bin/bash
 # Script to apply .Xresources at GUI startup
 # See ${PWD}/.config/autostart/autostart.desktop
 sleep 1
 xrdb -merge $HOME/.Xresources
-xdotool key shift+space
+# xdotool key shift+space
 exit
