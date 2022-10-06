065f14f5f47b97d3fdc3e9d6b81cedbce60dc0fd
Masashi Miyaura <masasam@users.noreply.github.com>
2022-10-07 07:09:40 +0900

    Fix typo

--- .emacs.d/inits/34java.el
+++ .emacs.d/inits/34java.el
@@ -3,8 +3,8 @@
 ;;; Code:
 ;;(setq debug-on-error t)
 
-(require 'lsp-java)
-(add-hook 'java-mode-hook 'lsp-deferred)
+;; (require 'lsp-java)
+;; (add-hook 'java-mode-hook 'lsp-deferred)
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
