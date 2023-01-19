73864350a74448413e67295c93bf1055aabbe198
Masashi Miyaura <masasam@users.noreply.github.com>
2023-01-19 12:54:15 +0900

    Fix typo

--- .emacs.d/inits/21python.el
+++ .emacs.d/inits/21python.el
@@ -3,9 +3,10 @@
 ;;; Code:
 ;;(setq debug-on-error t)
 
-(add-hook 'python-mode-hook (lambda ()
-                          (require 'lsp-pyright)
-                          (lsp-deferred)))
+(add-hook 'python-mode-hook 'eglot-ensure)
+;; (add-hook 'python-mode-hook (lambda ()
+;;                               (require 'lsp-pyright)
+;;                               (lsp-deferred)))
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
