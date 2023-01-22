a184438b2cfbfd56ba20b0036364e7a045c15aed
Masashi Miyaura <masasam@users.noreply.github.com>
2023-01-22 20:05:59 +0900

    Fix typo

--- .emacs.d/inits/26javascript.el
+++ .emacs.d/inits/26javascript.el
@@ -6,9 +6,9 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\.js$" . js2-mode))
 
-(add-hook 'js2-mode-hook 'lsp-deferred)
-(add-hook 'typescript-mode-hook 'lsp-deferred)
-(add-hook 'rjsx-mode-hook 'lsp-deferred)
+(add-hook 'js2-mode-hook 'eglot-ensure)
+(add-hook 'typescript-mode-hook 'eglot-ensure)
+(add-hook 'rjsx-mode-hook 'eglot-ensure)
 (add-hook 'rjsx-mode-hook
           (lambda ()
             (setq indent-tabs-mode nil)
