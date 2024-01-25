90696b312826f5af17ab31b7d93b6ea2a6a4edf0
Masashi Miyaura <masasam@users.noreply.github.com>
2024-01-26 04:54:21 +0900

    Fix typo

--- .emacs.d/inits/21python.el
+++ .emacs.d/inits/21python.el
@@ -3,17 +3,6 @@
 ;;; Code:
 ;;(setq debug-on-error t)
 
-(defun get-pdm-packages-path ()
-  "For the current PDM project, find the path to the packages."
-  (let ((packages-path (string-trim (shell-command-to-string "pdm info --packages"))))
-    (concat packages-path "/lib")))
-
-(defun my/eglot-workspace-config (server)
-  "For the current PDM project, dynamically generate a python lsp config."
-  `(:python\.analysis (:extraPaths ,(vector (get-pdm-packages-path)))))
-
-(setq-default eglot-workspace-configuration #'my/eglot-workspace-config)
-
 (setq python-indent-guess-indent-offset-verbose nil)
 
 (add-hook 'python-mode-hook 'eglot-ensure)
