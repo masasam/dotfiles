764462a6edd2eb3503408ddee58e540b4daec783
Masashi Miyaura <masasam@users.noreply.github.com>
2024-01-26 07:17:54 +0900

    Add eww setting

--- .emacs.d/inits/00base.el
+++ .emacs.d/inits/00base.el
@@ -267,7 +267,7 @@ If the region is inactive, `backward-kill-word'."
 
 (bind-key "C-M-y" 'my/duplicate-current-line)
 
-(setq eww-search-prefix "https://www.google.com/search?ie=UTF-8&q=")
+(setq eww-search-prefix "https://www.google.co.jp/search?q=")
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
