7a248d361785964ac5e7db4fdd695bb545e92f90
Masashi Miyaura <masasam@users.noreply.github.com>
2024-02-16 18:35:05 +0900

    Fix typo

--- .emacs.d/inits/00base.el
+++ .emacs.d/inits/00base.el
@@ -172,7 +172,7 @@ If the region is inactive, `backward-kill-word'."
 
 
 ;; emacs c source dir:
-(setq find-function-C-source-directory "~/backup/emacs/emacs-29.2/src")
+;; (setq find-function-C-source-directory "~/backup/emacs/emacs-29.2/src")
 
 
 ;; Do not change the position of the cursor on the screen as much as possible when scrolling pages
