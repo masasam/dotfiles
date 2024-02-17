75b5b75ebec81954eb3d1ec54eacf55a86bc9d9e
Masashi Miyaura <masasam@users.noreply.github.com>
2024-02-17 12:14:30 +0900

    Fix typo

--- .emacs.d/inits/21python.el
+++ .emacs.d/inits/21python.el
@@ -6,6 +6,8 @@
 (setq python-indent-guess-indent-offset-verbose nil)
 
 (add-hook 'python-mode-hook 'eglot-ensure)
+(add-hook 'eglot-managed-mode-hook 'flymake-ruff-load)
+
 ;; (add-hook 'python-mode-hook (lambda ()
 ;;                               (require 'lsp-pyright)
 ;;                               (lsp-deferred)))
