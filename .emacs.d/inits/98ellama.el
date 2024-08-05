628694d4a370cc734bddecd10a5e1a6dec570f6e
Masashi Miyaura <masasam@users.noreply.github.com>
2024-08-06 00:31:58 +0900

    Fix typo

---
+++ .emacs.d/inits/98ellama.el
@@ -0,0 +1,13 @@
+;;; 98ellama.el
+;;; Commentary:
+;;; Code:
+;;(setq debug-on-error t)
+
+(with-eval-after-load 'llm
+  (require 'llm-ollama)
+  (setopt ellama-language "Japanese"))
+
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
+;;; 34java ends here
