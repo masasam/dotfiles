9a55462af6f71434ebb8952eb01cfe36d76b7b7e
Masashi Miyaura <masasam@users.noreply.github.com>
2024-08-06 04:00:14 +0900

    Fix typo

--- .emacs.d/inits/98ellama.el
+++ .emacs.d/inits/98ellama.el
@@ -5,7 +5,11 @@
 
 (with-eval-after-load 'llm
   (require 'llm-ollama)
-  (setopt ellama-language "Japanese"))
+  (setopt ellama-language "Japanese")
+  (setopt ellama-translation-provider (make-llm-ollama
+                                       :chat-model "aya:8b"
+                                       :embedding-model "aya:8b"))
+)
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
