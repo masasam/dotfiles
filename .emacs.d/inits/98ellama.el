1e0028c537f7741255b27ec4b56e3762f755c363
Masashi Miyaura <masasam@users.noreply.github.com>
2024-10-04 20:27:25 +0900

    Fix typo

--- .emacs.d/inits/98ellama.el
+++ .emacs.d/inits/98ellama.el
@@ -9,7 +9,11 @@
   (setopt ellama-long-lines-length 80)
   (setopt ellama-provider (make-llm-ollama
                            :chat-model "lucas2024/gemma-2-2b-jpn-it:q8_0"
-                           :embedding-model "lucas2024/gemma-2-2b-jpn-it:q8_0")))
+                           :embedding-model "lucas2024/gemma-2-2b-jpn-it:q8_0"))
+  (setopt ellama-providers
+        '(("llama3.2" . (make-llm-ollama
+                         :chat-model "llama3.2"
+                         :embedding-model "llama3.2")))))
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
