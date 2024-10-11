ae8dc8b97fef56eef093f3a999675a9fe17595ff
Masashi Miyaura <masasam@users.noreply.github.com>
2024-10-11 17:33:54 +0900

    Fix typo

---
+++ .emacs.d/inits/26typescript.el
@@ -0,0 +1,11 @@
+;;; 26typescript.el --- 26typescript.el
+;;; Commentary:
+;;; Code:
+;;(setq debug-on-error t)
+
+(add-hook 'typescript-mode-hook #'lsp)
+
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
+;;; 26typescript.el ends here
