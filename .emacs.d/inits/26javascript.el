aa9d9d0912c2d65a08a0609ec47fb8748c529934
Masashi Miyaura <masasam@users.noreply.github.com>
2024-10-11 17:32:42 +0900

    Fix typo

--- .emacs.d/inits/26javascript.el
+++ .emacs.d/inits/26javascript.el
@@ -7,13 +7,6 @@
 (add-to-list 'auto-mode-alist '("\.js$" . js2-mode))
 
 (add-hook 'js2-mode-hook 'eglot-ensure)
-(add-hook 'typescript-mode-hook 'eglot-ensure)
-(add-hook 'rjsx-mode-hook 'eglot-ensure)
-(add-hook 'rjsx-mode-hook
-          (lambda ()
-            (setq indent-tabs-mode nil)
-            (setq js-indent-level 2)
-            (setq js2-strict-missing-semi-warning nil)))
 
 ;; (require 'lsp-mode)
 ;; (add-to-list 'auto-minor-mode-alist '("\.vue\\'" . lsp))
