128a45b98183781f5b70f3d24dbb5f6d9c22249c
Masashi Miyaura <masasam@users.noreply.github.com>
2024-10-17 08:15:58 +0900

    Fix typo

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,6 +4,12 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 config.automatically_reload_config = true
+config.font_size = 21
+config.window_background_opacity = 0.85
+config.macos_window_background_blur = 20
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
 
 -- This is where you actually apply your config choices
 
