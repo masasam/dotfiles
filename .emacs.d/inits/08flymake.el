1dcc284fd698398a90c7037898652e194363e08a
Masashi Miyaura <masasam@users.noreply.github.com>
2024-12-08 04:24:24 +0900

    Add puni

--- .emacs.d/inits/08flymake.el
+++ .emacs.d/inits/08flymake.el
@@ -48,15 +48,9 @@
 (add-hook 'text-mode-hook 'flyspell-mode)
 
 
-;; smartparens
-(require 'smartparens-config)
-(smartparens-global-mode t)
-(add-hook 'emacs-lisp-mode-hook 'turn-off-smartparens-mode)
-(add-hook 'lisp-interaction-mode-hook 'turn-off-smartparens-mode)
-(add-hook 'lisp-mode-hook 'turn-off-smartparens-mode)
-(add-hook 'ielm-mode-hook 'turn-off-smartparens-mode)
-(add-hook 'scheme-mode-hook 'turn-off-smartparens-mode)
-(add-hook 'slime-repl-mode-hook 'turn-off-smartparens-mode)
+(require 'puni)
+(puni-global-mode)
+(add-hook 'term-mode-hook #'puni-disable-puni-mode)
 
 
 ;; eldoc
