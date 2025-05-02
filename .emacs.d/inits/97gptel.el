35b9a5e26918a834f207f70e693c5c5c0e6d13c7
Masashi Miyaura <masasam@users.noreply.github.com>
2025-05-03 06:50:25 +0900

    Add gptel

---
+++ .emacs.d/inits/97gptel.el
@@ -0,0 +1,15 @@
+;;; package --- 97gptel.el
+;;; Commentary:
+;;; Code:
+;;(setq debug-on-error t)
+
+(setq gptel-model 'qwen3:30b-a3b
+      gptel-backend (gptel-make-ollama "Ollama"
+                      :host "localhost:11434"
+                      :stream t
+                      :models '(qwen3:30b-a3b)))
+
+  ;; Local Variables:
+  ;; byte-compile-warnings: (not free-vars)
+  ;; End:
+;;; 97gptel.el ends here
