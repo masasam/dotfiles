67f921ff6a65e23b10b3bb0bdc74f426e386bdd1
Masashi Miyaura <masasam@users.noreply.github.com>
2025-08-23 11:24:51 +0900

    Fix typo

--- .emacs.d/inits/26typescript.el
+++ .emacs.d/inits/26typescript.el
@@ -8,7 +8,9 @@
   (setq treesit-font-lock-level 4))
 
 (add-to-list 'auto-mode-alist (cons "\\.tsx\\'" 'tsx-ts-mode))
+(add-to-list 'auto-mode-alist (cons "\\.ts\\'" 'typescript-ts-mode))
 (add-hook 'tsx-ts-mode 'eglot-ensure)
+(add-hook 'typescript-ts-mode 'eglot-ensure)
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
