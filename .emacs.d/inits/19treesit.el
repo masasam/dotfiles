72d289388a689b7f879696de92f08f68da07f27e
Masashi Miyaura <masasam@users.noreply.github.com>
2025-09-04 20:34:09 +0900

    Fix typo

---
+++ .emacs.d/inits/19treesit.el
@@ -0,0 +1,13 @@
+;;; 19treesit.el --- 19treesit.el
+;;; Commentary:
+;;; Code:
+;;(setq debug-on-error t)
+
+(use-package treesit
+  :config
+  (setq treesit-font-lock-level 4))
+
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
+;;; 19treesit.el ends here
--- .emacs.d/inits/26typescript.el
+++ .emacs.d/inits/26typescript.el
@@ -3,23 +3,12 @@
 ;;; Code:
 ;;(setq debug-on-error t)
 
-(use-package treesit
-  :config
-  (setq treesit-font-lock-level 4))
-
-(use-package eglot
-  :ensure t
-  :hook
-  (tsx-ts-mode . eglot-ensure)
-  ;; :config
-  ;; (add-to-list 'eglot-server-programs '((bitbake-mode) "bitbake-language-server"))
-  )
-
 (use-package tsx-ts-mode
   :mode (("\\.ts[x]?\\'" . tsx-ts-mode)
          ("\\.[m]ts\\'" . tsx-ts-mode)
          ("\\.js[x]?\\'" . tsx-ts-mode)
-         ("\\.[mc]js\\'" . tsx-ts-mode)))
+         ("\\.[mc]js\\'" . tsx-ts-mode))
+  :hook (tsx-ts-mode . eglot-ensure))
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
