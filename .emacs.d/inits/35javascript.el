336b7c782652ae92a352689860f1bc6c4cdf6357
Masashi Miyaura <masasam@users.noreply.github.com>
2025-09-05 08:41:32 +0900

    Fix typo

--- .emacs.d/inits/35javascript.el
+++ .emacs.d/inits/35javascript.el
@@ -3,13 +3,11 @@
 ;;; Code:
 ;;(setq debug-on-error t)
 
-(autoload 'js2-mode "js2-mode" nil t)
-(add-to-list 'auto-mode-alist '("\.js$" . js2-mode))
+(add-to-list 'major-mode-remap-alist '(javascript-mode . js-ts-mode))
 
-(add-hook 'js2-mode-hook 'eglot-ensure)
-
-;; (require 'lsp-mode)
-;; (add-to-list 'auto-minor-mode-alist '("\.vue\\'" . lsp))
+(use-package js-ts-mode
+  :hook
+  (js-ts-mode . eglot-ensure))
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
