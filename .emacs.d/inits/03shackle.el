3ba3bd632599531b652236ee87f992806695731e
Masashi Miyaura <masasam@users.noreply.github.com>
2025-09-04 23:30:03 +0900

    Fix typo

--- .emacs.d/inits/03shackle.el
+++ .emacs.d/inits/03shackle.el
@@ -6,13 +6,17 @@
 (require 'shackle)
 (shackle-mode 1)
 (setq shackle-rules '(("*Help*" :align below :select t)
-		      ("*quickrun*" :align below :select t)
-		      ("*eldoc*" :align below :select t :size 15)
-		      ("*Ibuffer*" :align below :select t)
-		      ("*Shell Command Output*" :align below :select t)
-		      ("*vc-diff*" :align below :select t)
-		      ("*vc-change-log*" :align below :select t)
-		      ("*Google Translate*" :align below :select t)
-		      ("Trash Can" :align below :select t)))
+					  ("*quickrun*" :align below :select t)
+					  ("*eldoc*" :align below :select t)
+					  ("*eldoc for shackle-mode*" :align below :select t)
+					  ("*Ibuffer*" :align below :select t)
+					  ("*Shell Command Output*" :align below :select t)
+					  ("*vc-diff*" :align below :select t)
+					  ("*vc-change-log*" :align below :select t)
+					  ("*Google Translate*" :align below :select t)
+					  ("Trash Can" :align below :select t)))
 
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
 ;;; 03shackle.el ends here
