b3b3946b0de529bfe35b1d056547909e7b3660f0
Masashi Miyaura <masasam@users.noreply.github.com>
2025-09-06 12:46:39 +0900

    Fix typo

--- .emacs.d/inits/22cmode.el
+++ .emacs.d/inits/22cmode.el
@@ -4,14 +4,16 @@
 ;;(setq debug-on-error t)
 
 (use-package c-ts-mode
-  :mode ("\\.c\\'" . c-ts-mode)
   :hook (c-ts-mode . eglot-ensure)
   :init
+  (add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
   (add-hook 'c-mode-common-hook 'google-set-c-style))
 
 (use-package c++-ts-mode
-  :mode ("\\.cpp\\'" . c++-ts-mode)
-  :hook (c++-ts-mode . eglot-ensure))
+  :hook (c++-ts-mode . eglot-ensure)
+  :init
+  (add-to-list 'major-mode-remap-alist '(c++-mode . c++-ts-mode))
+  (add-to-list 'major-mode-remap-alist '(c-or-c++-mode . c-or-c++-ts-mode)))
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
