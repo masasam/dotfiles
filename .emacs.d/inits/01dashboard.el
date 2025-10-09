aa50669d10163cb36100df109d3eb13cb6a7d825
Masashi Miyaura <masasam@users.noreply.github.com>
2025-10-09 20:44:17 +0900

    Fix typo

--- .emacs.d/inits/01dashboard.el
+++ .emacs.d/inits/01dashboard.el
@@ -12,7 +12,7 @@
   (setq dashboard-banner-logo-title
 	(concat "GNU Emacs " emacs-version " kernel "
 		(car (split-string (shell-command-to-string "uname -r") "-"))
-		" x86_64 ArchLinux GTK " gtk-version-string))
+		" x86_64 ArchLinux Hyprland " (car (cdr (split-string (shell-command-to-string "hyprland --version") " ")))))
   ;; Set the banner
   (setq dashboard-startup-banner 'logo)
   (setq dashboard-items '((recents   . 5)))
