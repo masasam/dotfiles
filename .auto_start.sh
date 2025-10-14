12966b883c69e5391dbac3bb98e0dc8166a7c1d1
Masashi Miyaura <masasam@users.noreply.github.com>
2025-10-14 19:44:24 +0900

    Fix typo

--- .auto_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script to apply .Xresources at GUI startup
-# See ${PWD}/.config/autostart/autostart.desktop
-sleep 1
-xrdb -merge $HOME/.Xresources
-bluetoothctl power on
-# xdotool key shift+space
-exit
