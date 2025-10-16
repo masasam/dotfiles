f2947919f94f5f513181ffbced5f32bec8ca6464
Masashi Miyaura <masasam@users.noreply.github.com>
2025-10-16 14:58:54 +0900

    Fix typo

--- .config/waybar/scripts/start_recording.sh
+++ .config/waybar/scripts/start_recording.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pkill wl-screenrec || wl-screenrec -f "$(xdg-user-dir VIDEOS)/$(date +'%Y-%m-%d-%H%M%S.mp4')"
+pkill wl-screenrec || wl-screenrec -f "$(xdg-user-dir VIDEOS)/$(date +'%Y-%m-%d-%H%M%S.mp4')" &
