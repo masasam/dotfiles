7cb3129f9c55a459fff76ba32b1c39545f1ed13b
Masashi Miyaura <masasam@users.noreply.github.com>
2025-10-16 16:34:14 +0900

    Add copilot

---
+++ .emacs.d/inits/20copilot.el
@@ -0,0 +1,13 @@
+;;; 20copilot.el --- 20copilot.el
+;;; Commentary:
+;;; Code:
+;;(setq debug-on-error t)
+
+(use-package copilot
+  :bind
+  ((:map copilot-mode-map ("<tab>" . copilot-accept-completion))))
+
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
+;;; 20copilot.el ends here
