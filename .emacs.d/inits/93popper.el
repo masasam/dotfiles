8e8497c52b32c09b19454e99a6c3c166f63835c5
Masashi Miyaura <masasam@users.noreply.github.com>
2025-11-17 18:46:22 +0900

    Refactor Emacs window management
    Removed obsolete shackle configuration and replaced it with popper, configuring it to manage temporary buffers like *Messages*, output windows, and help buffers.
    
    * .emacs.d/inits/03shackle.el (deleted): Remove shackle configuration.
    * .emacs.d/inits/93popper.el (popper configuration): Add popper setup with keybindings and reference buffer list.

---
+++ .emacs.d/inits/93popper.el
@@ -0,0 +1,39 @@
+;;; 93popper.el --- 93popper.el
+;;; Commentary:
+;;; Code:
+;;(setq debug-on-error t)
+
+;; (require 'shackle)
+;; (shackle-mode 1)
+;; (setq shackle-rules '(("*Help*" :align below :select t)
+;; 					  ("*quickrun*" :align below :select t)
+;; 					  ("*eldoc*" :align below :select t)
+;; 					  ("*eldoc for shackle-mode*" :align below :select t)
+;; 					  ("*Ibuffer*" :align below :select t)
+;; 					  ("*Shell Command Output*" :align below :select t)
+;; 					  ("*vc-diff*" :align below :select t)
+;; 					  ("*vc-change-log*" :align below :select t)
+;; 					  ("*Google Translate*" :align below :select t)
+;; 					  ("Trash Can" :align below :select t)))
+
+(use-package popper
+  :ensure t ; or :straight t
+  :bind (("C-`"   . popper-toggle)
+         ("M-`"   . popper-cycle)
+         ("C-M-`" . popper-toggle-type))
+  :init
+  (setq popper-reference-buffers
+        '("\\*Messages\\*"
+          "Output\\*$"
+		  "\\*Google Translate\\*$"
+		  "\\*quickrun\\*"
+          "\\*Async Shell Command\\*"
+          help-mode
+          compilation-mode))
+  (popper-mode +1)
+  (popper-echo-mode +1))                ; For echo area hints
+
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
+;;; 93popper.el ends here
