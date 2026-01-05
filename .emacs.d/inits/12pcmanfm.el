74e7ec2d1ada46d61465dbd2651d566e0f1478d6
Masashi Miyaura <masasam@users.noreply.github.com>
2026-01-05 11:10:50 +0900

    Refactor: Replace nautilus emacs config with pcmanfm
    Removed redundant nautilus Elisp functions and replaced them with equivalent pcmanfm bindings, assuming a switch in file managers.
    
    * .emacs.d/inits/12nautilus.el (deleted): Remove old nautilus functions.
    * .emacs.d/inits/12pcmanfm.el (pcmanfm-open, pcmanfm-backup): Add new pcmanfm functions.

---
+++ .emacs.d/inits/12pcmanfm.el
@@ -0,0 +1,50 @@
+;;; 12pcmanfm.el --- 12pcmanfm.el
+;;; Commentary:
+;;; Code:
+;;(setq debug-on-error t)
+
+(defun pcmanfm-open ()
+  "Open current directry with pcmanfm."
+  (interactive)
+  (call-process-shell-command (concat "pcmanfm " default-directory)))
+
+
+(defun pcmanfm-backup ()
+  "Open backup directry with pcmanfm."
+  (interactive)
+  (call-process-shell-command "pcmanfm ~/backup"))
+
+
+(defun pcmanfm-work ()
+  "Open work directry with pcmanfm."
+  (interactive)
+  (call-process-shell-command "pcmanfm ~/backup/work"))
+
+
+(defun pcmanfm-cash ()
+  "Open cash directry with pcmanfm."
+  (interactive)
+  (call-process-shell-command "pcmanfm ~/backup/cash"))
+
+
+(defun pcmanfm-downloads ()
+  "Open downloads directry with pcmanfm."
+  (interactive)
+  (call-process-shell-command "pcmanfm ~/Downloads"))
+
+
+(defun pcmanfm-documents ()
+  "Open documents directry with pcmanfm."
+  (interactive)
+  (call-process-shell-command "pcmanfm ~/Documents"))
+
+
+(defun pcmanfm-pictures ()
+  "Open pictures directry with pcmanfm."
+  (interactive)
+  (call-process-shell-command "pcmanfm ~/Pictures"))
+
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
+;;; 12pcmanfm.el ends here
