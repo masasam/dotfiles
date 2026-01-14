07eef1a248bdb06703dbab3ce17ee90fe49ca8db
Masashi Miyaura <masasam@users.noreply.github.com>
2026-01-14 09:18:15 +0900

    .emacs.d/inits/03consult.el: Fix consult-source naming consistency

--- .emacs.d/inits/03consult.el
+++ .emacs.d/inits/03consult.el
@@ -97,8 +97,8 @@
    consult-theme :preview-key '(:debounce 0.2 any)
    consult-ripgrep consult-git-grep consult-grep consult-man
    consult-bookmark consult-recent-file consult-xref
-   consult--source-bookmark consult--source-file-register
-   consult--source-recent-file consult--source-project-recent-file
+   consult-source-bookmark consult-source-file-register
+   consult-source-recent-file consult-source-project-recent-file
    ;; :preview-key "M-."
    :preview-key '(:debounce 0.4 any))
 
