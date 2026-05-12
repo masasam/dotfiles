f55affe632617a10593e130895364d0d309b4712
Masashi Miyaura <masasam@users.noreply.github.com>
2026-05-13 07:53:18 +0900

    .config/hypr/hyprland.lua: Enable workspace switching and moving.

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -299,11 +299,11 @@ hl.bind("SUPER + j", hl.dsp.focus({ direction = "down" }))
 
 -- Switch workspaces with mainMod + [0-9]
 -- Move active window to a workspace with mainMod + SHIFT + [0-9]
--- for i = 1, 10 do
---    local key = i % 10 -- 10 maps to key 0
---    hl.bind(mainMod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
---    hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace -- i }))
--- end
+for i = 1, 10 do
+    local key = i % 10 -- 10 maps to key 0
+    hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i}))
+    hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
+end
 
 -- Example special workspace (scratchpad)
 hl.bind(mainMod .. " + Return", hl.dsp.workspace.toggle_special("magic"))
