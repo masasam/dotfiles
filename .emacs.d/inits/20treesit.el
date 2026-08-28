70ff6439c3243fb97a384945e3cd65a33e0bd442
Masashi Miyaura <masasam@users.noreply.github.com>
2026-08-28 11:31:39 +0900

    Update packages and tree-sitter configuration
    * .emacs.d/inits/20treesit.el (treesit): Auto-install grammars on Emacs 31+.
    * archlinux/aurlist: Add yacreader.
    * archlinux/pacmanlist: Update installed packages.

--- .emacs.d/inits/20treesit.el
+++ .emacs.d/inits/20treesit.el
@@ -5,7 +5,10 @@
 
 (use-package treesit
   :config
-  (setq treesit-font-lock-level 4))
+  (setq treesit-font-lock-level 4)
+  (when (> emacs-major-version 30)
+    (setq treesit-auto-install-grammar 'always
+          treesit-enabled-modes t)))
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
