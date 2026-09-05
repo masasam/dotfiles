b3d6c000c8279e6db0b5e0fb2986ac8fac689cd1
Masashi Miyaura <masasam@users.noreply.github.com>
2026-09-05 10:20:25 +0900

    .emacs.d/inits/27rust.el (my/rust-eglot-ensure, my/project-try-cargo): Detect Cargo workspace root for eglot.

--- .emacs.d/inits/27rust.el
+++ .emacs.d/inits/27rust.el
@@ -3,9 +3,19 @@
 ;;; Code:
 ;;(setq debug-on-error t)
 
+(defun my/project-try-cargo (directory)
+  "Return a project rooted at the nearest Cargo.toml above DIRECTORY."
+  (when-let* ((root (locate-dominating-file directory "Cargo.toml")))
+    (cons 'transient root)))
+
+(defun my/rust-eglot-ensure ()
+  "Start Eglot with the nearest Cargo package as its project root."
+  (add-hook 'project-find-functions #'my/project-try-cargo nil t)
+  (eglot-ensure))
+
 (use-package rust-ts-mode
   :mode ("\\.rs\\'" . rust-ts-mode)
-  :hook (rust-ts-mode . eglot-ensure))
+  :hook (rust-ts-mode . my/rust-eglot-ensure))
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
